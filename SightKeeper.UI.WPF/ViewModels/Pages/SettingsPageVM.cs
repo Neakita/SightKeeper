23eb1fbf7b5dd8847e41aedd694b2018098e6bef
KatterMaw <TheKatterDev@gmail.com>
2022-09-10 16:45:35 +0500

    Create settings page with games setup

---
+++ SightKeeper.UI.WPF/ViewModels/Pages/SettingsPageVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using ReactiveUI;
+using SightKeeper.Backend.Data;
+using SightKeeper.Backend.Data.Members;
+
+namespace SightKeeper.UI.WPF.ViewModels.Pages;
+
+public class SettingsPageVM : ReactiveObject
+{
+	public Game[] Games
+	{
+		get
+		{
+			using AppDbContext dbContext = new();
+			return Games.ToArray();
+		}
+	}
+
+	public Process[] Processes => Process.GetProcesses().Where(process => process.MainWindowHandle != IntPtr.Zero).ToArray();
+}
---
+++ SightKeeper.UI.WPF/Views/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SightKeeper.UI.WPF.Views.Pages;
+
+public partial class SettingsPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+}
