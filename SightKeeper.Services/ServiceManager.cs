b9a7bd7d0f5082c02577ed0f03c2c78c620eb281
KatterMaw <TheKatterDev@gmail.com>
2022-09-16 11:36:37 +0500

    Implemented DI provider

---
+++ SightKeeper.Services/ServiceManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SightKeeper.Services.Registrators;
+
+namespace SightKeeper.Services;
+
+public static class ServiceManager
+{
+	public static T GetService<T>() where T : notnull => Host.Services.GetRequiredService<T>();
+
+
+	private static IHost BuildServices(string[]? args = null)
+	{
+		IHostBuilder? builder = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args);
+		builder.ConfigureServices(InitializeServices);
+		return builder.Build();
+	}
+	
+	
+	private static IHost? _host;
+	private static IHost Host => _host ??= BuildServices();
+	
+	
+	private static void InitializeServices(this IServiceCollection services) =>
+		services.AddDatabase().AddDbRepositories();
+}
