fa0dd287ee9976b0f3663394481db569739b85e8
KatterMaw <TheKatterDev@gmail.com>
2023-01-03 22:17:57 +0500

    Disable Hamburger menu hold selection

--- SightKeeper.UI.WPF/ViewModels/Windows/MainWindowVM.cs
+++ SightKeeper.UI.WPF/ViewModels/Windows/MainWindowVM.cs
@@ -1,11 +1,23 @@
-﻿using ReactiveUI;
+﻿using System.Reactive.Linq;
+using System.Windows.Controls;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 using SightKeeper.UI.WPF.ViewModels.Elements;
 
 namespace SightKeeper.UI.WPF.ViewModels.Windows;
 
 public sealed class MainWindowVM : ReactiveObject
 {
-	public MainWindowVM(HamburgerMenuVM hamburgerMenuVM) => HamburgerMenuVM = hamburgerMenuVM;
+	public MainWindowVM(HamburgerMenuVM hamburgerMenuVM)
+	{
+		HamburgerMenuVM = hamburgerMenuVM;
+		
+		HamburgerMenuVM.WhenAnyValue(vm => vm.SelectedMenuItem)
+			.Select(menuItem => menuItem.Content)
+			.ToPropertyEx(this, vm => vm.Content);
+	}
 
 	public HamburgerMenuVM HamburgerMenuVM { get; }
+
+	[ObservableAsProperty] public Control Content { get; } = null!;
 }
