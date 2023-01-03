0f9d7d729e61bd5e253c0737e8b6e3d3f8e295cf
KatterMaw <TheKatterDev@gmail.com>
2023-01-03 20:49:09 +0500

    Move some logic from code to xaml

--- SightKeeper.UI.WPF/ViewModels/Elements/HamburgerMenuVM.cs
+++ SightKeeper.UI.WPF/ViewModels/Elements/HamburgerMenuVM.cs
@@ -9,8 +9,6 @@ namespace SightKeeper.UI.WPF.ViewModels.Elements;
 
 public sealed class HamburgerMenuVM : ReactiveObject
 {
-	private HamburgerMenuItem _selectedMenuItem;
-
 	public HamburgerMenuVM(ModelsPage modelsPage)
 	{
 		MenuItems = new HamburgerMenuItem[]
@@ -20,27 +18,12 @@ public sealed class HamburgerMenuVM : ReactiveObject
 			new("Models", modelsPage, PackIconBootstrapIconsKind.Grid,
 				PackIconBootstrapIconsKind.GridFill)
 		};
-
-		_selectedMenuItem = MenuItems.First();
-		_selectedMenuItem.IsSelected = true;
+		SelectedMenuItem = MenuItems.First();
 	}
 
 	[Reactive] public bool IsExpanded { get; set; }
 	
 	public IEnumerable<HamburgerMenuItem> MenuItems { get; }
 
-	public IEnumerable<HamburgerMenuItem> OptionsMenuItems { get; } =
-		Enumerable.Empty<HamburgerMenuItem>();
-
-	public HamburgerMenuItem SelectedMenuItem
-	{
-		get => _selectedMenuItem;
-		set
-		{
-			_selectedMenuItem.IsSelected = false;
-			_selectedMenuItem = value;
-			_selectedMenuItem.IsSelected = true;
-			this.RaisePropertyChanged();
-		}
-	}
+	[Reactive] public HamburgerMenuItem SelectedMenuItem { get; set; }
 }
