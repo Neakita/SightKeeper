202ac9b4030fe626dda78f23440d2bdf99421461
KatterMaw <TheKatterDev@gmail.com>
2023-04-09 14:50:53 +0500

    Performance enchantments in model screenshoter

--- SightKeeper.Infrastructure.Services/OnShootModelScreenshoter.cs
+++ SightKeeper.Infrastructure.Services/OnShootModelScreenshoter.cs
@@ -6,6 +6,7 @@ using SightKeeper.Application.Annotating;
 using SightKeeper.Domain.Model.Abstract;
 using SightKeeper.Domain.Model.Common;
 using SightKeeper.Domain.Model.Detector;
+using SightKeeper.Infrastructure.Common;
 using SightKeeper.Infrastructure.Data;
 using MouseButton = SharpHook.Native.MouseButton;
 
@@ -22,7 +23,7 @@ public sealed class OnShootModelScreenshoter : ReactiveObject, ModelScreenshoter
 			this.RaiseAndSetIfChanged(ref _model, value);
 			_detectorModel = value as DetectorModel;
 			if (_model == null) return;
-			LoadScreenshots(_model);
+			_ = LoadScreenshotsAsync(_model);
 			_screenCapture.Resolution = _model.Resolution;
 		}
 	}
@@ -54,29 +55,33 @@ public sealed class OnShootModelScreenshoter : ReactiveObject, ModelScreenshoter
 
 	[Reactive] public ushort MaxImages { get; set; } = 500;
 
-	public OnShootModelScreenshoter(ScreenCapture screenCapture, KeyHook keyHook, AppDbContextFactory dbContextFactory, ScreenBoundsProvider screenBoundsProvider)
+	public OnShootModelScreenshoter(ScreenCapture screenCapture, KeyHook keyHook, AppDbContextFactory dbContextFactory)
 	{
 		_screenCapture = screenCapture;
 		_keyHook = keyHook;
 		_dbContextFactory = dbContextFactory;
-		_screenBoundsProvider = screenBoundsProvider;
 		_interval = 1000 / FramesPerSecond;
 	}
 
 	private readonly ScreenCapture _screenCapture;
 	private readonly KeyHook _keyHook;
 	private readonly AppDbContextFactory _dbContextFactory;
-	private readonly ScreenBoundsProvider _screenBoundsProvider;
 	private Model? _model;
 	private DetectorModel? _detectorModel;
 	private bool _isEnabled;
 	private byte _onHoldFPS = 1;
 	private int _interval;
+	private bool _capturing;
 
 	private void KeyHookOnMouseButtonPressed(KeyHook sender, MouseButton button)
 	{
 		if (button != MouseButton.Button1) return;
 		if (Model == null) return;
+		if (_capturing) return;
+		_detectorModel.ThrowIfNull(nameof(_detectorModel));
+		_capturing = true;
+		using AppDbContext dbContext = _dbContextFactory.CreateDbContext();
+		dbContext.Attach(_detectorModel!);
 		while (sender.IsPressed(button))
 		{
 			lock (this)
@@ -85,6 +90,9 @@ public sealed class OnShootModelScreenshoter : ReactiveObject, ModelScreenshoter
 			}
 			Thread.Sleep(_interval);
 		}
+		DeleteExceedScreenshots();
+		dbContext.SaveChanges();
+		_capturing = false;
 	}
 
 	private void Capture()
@@ -93,35 +101,27 @@ public sealed class OnShootModelScreenshoter : ReactiveObject, ModelScreenshoter
 		byte[] bytes = _screenCapture.Capture();
 		Image image = new(bytes);
 		DetectorScreenshot screenshot = new(_detectorModel, image);
-		using AppDbContext dbContext = _dbContextFactory.CreateDbContext();
-		dbContext.Update(_detectorModel);
 		_detectorModel.DetectorScreenshots.Add(screenshot);
-		dbContext.SaveChanges();
-		DeleteExceedScreenshots();
 	}
 
 	private void DeleteExceedScreenshots()
 	{
-		if (_detectorModel == null) throw new InvalidOperationException("Detector model is null");
-		if (_detectorModel.DetectorScreenshots.Count > MaxImages)
-		{
-			int imagesToDelete = MaxImages - _detectorModel.DetectorScreenshots.Count;
-			using AppDbContext dbContext = _dbContextFactory.CreateDbContext();
-			dbContext.Update(_detectorModel);
-			for (int i = 0; i < imagesToDelete; i++)
-				_detectorModel.DetectorScreenshots.RemoveAt(0);
-			dbContext.SaveChanges();
-		}
+		_detectorModel.ThrowIfNull(nameof(_detectorModel));
+		List<int> notAssetsIndexes = _detectorModel!.DetectorScreenshots.Select((screenshot, index) => (screenshot, index)).Where(item => !item.screenshot.IsAsset).Select(item => item.index).ToList();
+		if (notAssetsIndexes.Count <= MaxImages) return;
+		IOrderedEnumerable<int> indexesToRemove = notAssetsIndexes.Take(notAssetsIndexes.Count - MaxImages).OrderDescending();
+		foreach (int index in indexesToRemove)
+			_detectorModel.DetectorScreenshots.RemoveAt(index);
 	}
 
-	private void LoadScreenshots(Model model)
+	private async Task LoadScreenshotsAsync(Model model)
 	{
 		if (model is DetectorModel detectorModel)
 		{
-			using AppDbContext dbContext = _dbContextFactory.CreateDbContext();
-			dbContext.Update(model);
-			dbContext.Entry(detectorModel).Collection(m => m.DetectorScreenshots).Load();
-			dbContext.DetectorScreenshots.Where(screenshot => screenshot.Model == model).Select(screenshot => screenshot.Image).Load();
+			await using AppDbContext dbContext = await _dbContextFactory.CreateDbContextAsync();
+			dbContext.Attach(model);
+			await dbContext.Entry(detectorModel).Collection(m => m.DetectorScreenshots).LoadAsync();
+			await dbContext.DetectorScreenshots.Where(screenshot => screenshot.Model == model).Select(screenshot => screenshot.Image).LoadAsync();
 		}
 		else throw new InvalidCastException();
 	}
