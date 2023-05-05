6d57412afe1fd70e366ac5bb2814e6136e5f8bd8
KatterMaw <TheKatterDev@gmail.com>
2023-05-05 19:54:31 +0500

    Db migration

---
+++ SightKeeper.Data/Migrations/20230504153452_SpecifyModelInItemClassMigration.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SightKeeper.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SpecifyModelInItemClassMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ItemClasses_Models_ModelId",
+                table: "ItemClasses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModelId",
+                table: "ItemClasses",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "DetectorItems",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ItemClasses_Models_ModelId",
+                table: "ItemClasses",
+                column: "ModelId",
+                principalTable: "Models",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ItemClasses_Models_ModelId",
+                table: "ItemClasses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModelId",
+                table: "ItemClasses",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "DetectorItems",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ItemClasses_Models_ModelId",
+                table: "ItemClasses",
+                column: "ModelId",
+                principalTable: "Models",
+                principalColumn: "Id");
+        }
+    }
+}
