5181688b52b75c31031a2864963552211e5f1020
KatterMaw <TheKatterDev@gmail.com>
2023-05-05 19:54:07 +0500

    Model editing bug, added unit test for future fix

--- SightKeeper.Data.Tests/AppDbContextTests.cs
+++ SightKeeper.Data.Tests/AppDbContextTests.cs
@@ -51,4 +51,28 @@ public sealed class AppDbContextTests : DbRelatedTests
 		
 		profile.Name.Should().Be(originTestProfileName);
 	}
+
+	[Fact]
+	public void ShouldRollbackEntityCollection()
+	{
+		DetectorModel model = new("Test model");
+		ItemClass firstItemClass = new("First");
+		model.ItemClasses.Add(firstItemClass);
+		using (AppDbContext dbContext = DbContextFactory.CreateDbContext())
+		{
+			dbContext.DetectorModels.Add(model);
+			dbContext.SaveChanges();
+			dbContext.DetectorModels.Should().Contain(model);
+			dbContext.ItemClasses.Should().Contain(firstItemClass);
+		}
+
+		using (AppDbContext dbContext = DbContextFactory.CreateDbContext())
+		{
+			dbContext.Attach(model);
+			ItemClass secondItemClass = new("Second");
+			model.ItemClasses.Add(secondItemClass);
+			dbContext.RollBack(model);
+			model.ItemClasses.Should().OnlyContain(item => item == firstItemClass);
+		}
+	}
 }
