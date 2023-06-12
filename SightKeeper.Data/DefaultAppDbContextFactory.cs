e7cb031ca8783689a91d9871fe5889ad7a878060
KatterMaw <TheKatterDev@gmail.com>
2023-06-12 16:07:09 +0500

    Change migrate to ensure created in DefaultAppDbContextFactory

--- SightKeeper.Data/DefaultAppDbContextFactory.cs
+++ SightKeeper.Data/DefaultAppDbContextFactory.cs
@@ -1,20 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace SightKeeper.Data;
+﻿namespace SightKeeper.Data;
 
 public sealed class DefaultAppDbContextFactory : AppDbContextFactory
 {
 	public AppDbContext CreateDbContext()
 	{
 		AppDbContext dbContext = new();
-		dbContext.Database.Migrate();
+		dbContext.Database.EnsureCreated();
 		return dbContext;
 	}
 
 	public async Task<AppDbContext> CreateDbContextAsync(CancellationToken cancellationToken = default)
 	{
 		AppDbContext dbContext = new();
-		await dbContext.Database.MigrateAsync(cancellationToken);
+		await dbContext.Database.EnsureCreatedAsync(cancellationToken);
 		return dbContext;
 	}
 }
