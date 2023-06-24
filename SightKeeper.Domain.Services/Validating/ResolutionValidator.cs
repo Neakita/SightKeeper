b815902494d80fb6b501a0dbfaae42788ed33a72
KatterMaw <narcat99@gmail.com>
2023-06-25 00:18:11 +0500

    Validate what resolution width and height is less than or equal to 32000 so it can be represented as short and ushort

--- SightKeeper.Domain.Services/Validating/ResolutionValidator.cs
+++ SightKeeper.Domain.Services/Validating/ResolutionValidator.cs
@@ -9,9 +9,11 @@ public sealed class ResolutionValidator : AbstractValidator<IResolution>
     {
         RuleFor(resolution => resolution.Width)
             .Must(width => width > 0).WithMessage("{PropertyName} must be greater than 0")
-            .Must(width => width % 32 == 0).WithMessage("{PropertyName} must be a multiple of 32");
+            .Must(width => width % 32 == 0).WithMessage("{PropertyName} must be a multiple of 32")
+            .Must(width => width <= 32000).WithMessage("{PropertyName} must be less than or equal to 32000");
         RuleFor(resolution => resolution.Height)
             .Must(height => height > 0).WithMessage("{PropertyName} must be greater than 0")
-            .Must(height => height % 32 == 0).WithMessage("{PropertyName} must be a multiple of 32");
+            .Must(height => height % 32 == 0).WithMessage("{PropertyName} must be a multiple of 32")
+            .Must(height => height <= 32000).WithMessage("{PropertyName} must be less than or equal to 32000");
     }
 }
