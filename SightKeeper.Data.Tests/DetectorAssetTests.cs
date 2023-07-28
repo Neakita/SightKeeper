6aff92dc4a5e9e76754338268dd221527ce8570b
KatterMaw <narcat99@gmail.com>
2023-07-28 22:06:30 +0500

    Move ShouldLoadAssetOfScreenshot test to ScreenshotTests.cs

--- SightKeeper.Data.Tests/DetectorAssetTests.cs
+++ SightKeeper.Data.Tests/DetectorAssetTests.cs
@@ -51,23 +51,4 @@ public sealed class DetectorAssetTests : DbRelatedTests
         model.ScreenshotsLibrary.Screenshots.Should().NotBeEmpty();
         model.Assets.Should().NotBeEmpty();
     }
-
-    [Fact]
-    public void ShouldLoadAssetOfScreenshot()
-    {
-        using (var arrangeDbContext = DbContextFactory.CreateDbContext())
-        {
-            DetectorModel model = new("Test model");
-            var screenshot = model.ScreenshotsLibrary.CreateScreenshot(Array.Empty<byte>());
-            model.MakeAssetFromScreenshot(screenshot);
-            arrangeDbContext.Add(model);
-            arrangeDbContext.SaveChanges();
-        }
-        using (var assertDbContext = DbContextFactory.CreateDbContext())
-        {
-            var modelFromDb = assertDbContext.DetectorModels.Include(model => model.ScreenshotsLibrary.Screenshots).ThenInclude(screenshot => screenshot.Asset).Single();
-            var screenshot = modelFromDb.ScreenshotsLibrary.Screenshots.Single();
-            screenshot.Asset.Should().NotBeNull();
-        }
-    }
 }
---
+++ SightKeeper.Data.Tests/ScreenshotTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SightKeeper.Domain.Model.Detector;
+using SightKeeper.Tests.Common;
+
+namespace SightKeeper.Data.Tests;
+
+public sealed class ScreenshotTests : DbRelatedTests
+{
+    [Fact]
+    public void ShouldLoadAssetOfScreenshot()
+    {
+        using (var arrangeDbContext = DbContextFactory.CreateDbContext())
+        {
+            DetectorModel model = new("Test model");
+            var screenshot = model.ScreenshotsLibrary.CreateScreenshot(Array.Empty<byte>());
+            model.MakeAssetFromScreenshot(screenshot);
+            arrangeDbContext.Add(model);
+            arrangeDbContext.SaveChanges();
+        }
+        using (var assertDbContext = DbContextFactory.CreateDbContext())
+        {
+            var modelFromDb = assertDbContext.DetectorModels.Include(model => model.ScreenshotsLibrary.Screenshots).ThenInclude(screenshot => screenshot.Asset).Single();
+            var screenshot = modelFromDb.ScreenshotsLibrary.Screenshots.Single();
+            screenshot.Asset.Should().NotBeNull();
+        }
+    }
+}
