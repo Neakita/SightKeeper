eb7c5e58b42f7f63df56833914314cb6c2d0775b
KatterMaw <narcat99@gmail.com>
2023-08-03 14:53:38 +0500

    Make ItemClass.DetectorItems IReadOnlyCollection, add internal methods AddDetectorItem & RemoveDetectorItem, so editing restricted to domain only

--- SightKeeper.Domain.Model/Common/ItemClass.cs
+++ SightKeeper.Domain.Model/Common/ItemClass.cs
@@ -6,21 +6,26 @@ public sealed class ItemClass
 {
 	public Model Model { get; private set; }
 	public string Name { get; set; }
-	public ICollection<DetectorItem> DetectorItems { get; internal set; }
+	public IReadOnlyCollection<DetectorItem> DetectorItems => _detectorItems;
 
 	internal ItemClass(Model model, string name)
 	{
 		Model = model;
 		Name = name;
-		DetectorItems = new List<DetectorItem>();
+		_detectorItems = new List<DetectorItem>();
 	}
 
+	internal void AddDetectorItem(DetectorItem item) => _detectorItems.Add(item);
+	internal void RemoveDetectorItem(DetectorItem item) => _detectorItems.Remove(item);
+
 	public override string ToString() => Name;
+
+	private readonly List<DetectorItem> _detectorItems;
 
 	private ItemClass(string name)
 	{
 		Model = null!;
 		Name = name;
-		DetectorItems = null!;
+		_detectorItems = null!;
 	}
 }
--- SightKeeper.Domain.Model/Detector/DetectorAsset.cs
+++ SightKeeper.Domain.Model/Detector/DetectorAsset.cs
@@ -22,7 +22,7 @@ public sealed class DetectorAsset : Asset
 			ThrowHelper.ThrowInvalidOperationException($"Model \"{Model}\" does not contain item class \"{itemClass}\"");
 		DetectorItem item = new(this, itemClass, boundingBox);
 		_items.Add(item);
-		itemClass.DetectorItems.Add(item);
+		itemClass.AddDetectorItem(item);
 		return item;
 	}
 
@@ -30,7 +30,7 @@ public sealed class DetectorAsset : Asset
 	{
 		if (!_items.Remove(item))
 			ThrowHelper.ThrowArgumentException(nameof(item), "Item not found");
-		item.ItemClass.DetectorItems.Remove(item);
+		item.ItemClass.RemoveDetectorItem(item);
 	}
 
 	public void DeleteItems(IEnumerable<DetectorItem> items)
@@ -39,14 +39,14 @@ public sealed class DetectorAsset : Asset
 		{
 			if (!_items.Remove(item))
 				ThrowHelper.ThrowArgumentException(nameof(items), "One or more items not found");
-			item.ItemClass.DetectorItems.Remove(item);
+			item.ItemClass.RemoveDetectorItem(item);
 		}
 	}
 
 	public void ClearItems()
 	{
 		foreach (var detectorItem in _items)
-			detectorItem.ItemClass.DetectorItems.Remove(detectorItem);
+			detectorItem.ItemClass.RemoveDetectorItem(detectorItem);
 		_items.Clear();
 	}
 
