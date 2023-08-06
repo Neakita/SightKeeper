29137cdb9bf5562627422ceaa2cd4cb47e63434e
KatterMaw <narcat99@gmail.com>
2023-08-06 08:27:18 +0500

    Create "ScreenshotsLibraryHasAnyScreenshotsColumn" migration

---
+++ SightKeeper.Data/Migrations/20230806031707_ScreenshotsLibraryHasAnyScreenshotsColumn.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SightKeeper.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScreenshotsLibraryHasAnyScreenshotsColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasAnyScreenshots",
+                table: "ScreenshotsLibraries",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+            
+            migrationBuilder.Sql(@"UPDATE ScreenshotsLibraries
+SET HasAnyScreenshots = (SELECT COUNT(*) > 0 FROM Screenshots WHERE LibraryId = ScreenshotsLibraries.Id);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasAnyScreenshots",
+                table: "ScreenshotsLibraries");
+        }
+    }
+}
