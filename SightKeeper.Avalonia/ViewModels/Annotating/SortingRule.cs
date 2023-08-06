593a05bf37b7ef9d36a47d08b8290b56497b93b4
KatterMaw <narcat99@gmail.com>
2023-08-06 06:20:22 +0500

    Remove sort direction icon

--- SightKeeper.Avalonia/ViewModels/Annotating/SortingRule.cs
+++ SightKeeper.Avalonia/ViewModels/Annotating/SortingRule.cs
@@ -7,15 +7,15 @@ namespace SightKeeper.Avalonia.ViewModels.Annotating;
 public sealed class SortingRule<T>
 {
     public string Name { get; }
-    public SortDirection Direction { get; }
     public IComparer<T> Comparer { get; }
 
     public SortingRule(string name, SortDirection direction, Func<T, IComparable> expression)
     {
         Name = name;
-        Direction = direction;
         Comparer = direction == SortDirection.Ascending
             ? SortExpressionComparer<T>.Ascending(expression)
             : SortExpressionComparer<T>.Descending(expression);
     }
+
+    public override string ToString() => Name;
 }
