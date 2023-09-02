4b20ad923c20b4121d3bdbd1c484a8c4120ae23c
KatterMaw <narcat99@gmail.com>
2023-09-02 11:46:47 +0500

    Fix Design time view model for DataSetEditor

--- SightKeeper.Avalonia/ViewModels/Fakes/FakeDataSetCreatingViewModel.cs
+++ SightKeeper.Avalonia/ViewModels/Fakes/FakeDataSetCreatingViewModel.cs
@@ -1,14 +1,33 @@
-﻿using FluentValidation;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using NSubstitute;
-using SightKeeper.Application.DataSet.Creating;
-using SightKeeper.Services.Games;
+using SightKeeper.Avalonia.ViewModels.Dialogs;
+using SightKeeper.Domain.Model.Common;
 
 namespace SightKeeper.Avalonia.ViewModels.Fakes;
 
-public sealed class FakeDataSetCreatingViewModel : Dialogs.DataSetCreatingViewModel
+public sealed class FakeDataSetCreatingViewModel : IDataSetEditorViewModel
 {
-    public FakeDataSetCreatingViewModel() : base(Substitute.For<IValidator<NewDataSetInfo>>(), Substitute.For<RegisteredGamesService>())
+    public FakeDataSetCreatingViewModel()
     {
         Name = "Some data set";
     }
+
+    public event PropertyChangingEventHandler? PropertyChanging;
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public string Name { get; set; }
+    public string Description { get; set; } = "Some Description... more words and all these things";
+    public int? Resolution { get; set; } = 320;
+    public IReadOnlyCollection<string> ItemClasses { get; } = new []{ "Item class 1", "Item class 2" };
+    public string? SelectedItemClass { get; set; }
+    public string NewItemClassName { get; set; }
+    public Game? Game { get; set; }
+    public Task<IReadOnlyCollection<Game>> Games { get; } = Task.FromResult((IReadOnlyCollection<Game>)Array.Empty<Game>());
+    public ICommand AddItemClassCommand { get; } = Substitute.For<ICommand>();
+    public ICommand DeleteItemClassCommand { get; } = Substitute.For<ICommand>();
+    public ICommand ApplyCommand { get; } = Substitute.For<ICommand>();
+    public ICommand CancelCommand { get; } = Substitute.For<ICommand>();
 }
