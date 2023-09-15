0cd97e244668c553ccf5380df5c454597ff3e632
KatterMaw <narcat99@gmail.com>
2023-09-15 15:49:39 +0500

    Sketch of profile class and it's aggregates related to item classes grouping

---
+++ SightKeeper.Domain.Model/Profile/OrderedItemClass.cs
@@ -0,0 +1,15 @@
+﻿using SightKeeper.Domain.Model.Common;
+
+namespace SightKeeper.Domain.Model;
+
+public sealed class OrderedItemClass
+{
+    public ItemClass ItemClass { get; private set; }
+    public int Index { get; set; }
+
+    public OrderedItemClass(ItemClass itemClass, int index)
+    {
+        ItemClass = itemClass;
+        Index = index;
+    }
+}
---
+++ SightKeeper.Domain.Model/Profile/Profile.cs
@@ -0,0 +1,76 @@
+﻿using CommunityToolkit.Diagnostics;
+using SightKeeper.Domain.Model.Common;
+
+namespace SightKeeper.Domain.Model;
+
+public sealed class Profile
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public DataSet DataSet
+    {
+        get => _dataSet;
+        set
+        {
+            if (_dataSet == value)
+                return;
+            _dataSet = value;
+            ClearGroups();
+        }
+    }
+
+    public IReadOnlyList<ItemClassesGroup> ItemClassesGroups => _itemClassesGroups;
+
+    public Profile(string name, string description, DataSet dataSet)
+    {
+        Name = name;
+        Description = description;
+        _dataSet = dataSet;
+        _itemClassesGroups = new List<ItemClassesGroup>();
+    }
+
+    public ItemClassesGroup CreateGroup(string name)
+    {
+        ItemClassesGroup group = new(name);
+        _itemClassesGroups.Add(group);
+        return group;
+    }
+
+    public void AssignItemClassToGroup(ItemClass itemClass, ItemClassesGroup group)
+    {
+        if (!DataSet.ItemClasses.Contains(itemClass))
+            ThrowHelper.ThrowArgumentException($"Item class \"{itemClass}\" not found in dataset \"{DataSet}\"");
+        if (!ItemClassesGroups.Contains(group))
+            ThrowHelper.ThrowArgumentException($"Group \"{group}\" not found");
+        group.AddItemClass(itemClass);
+    }
+    
+    public void UnAssignItemClassFromGroup(ItemClass itemClass, ItemClassesGroup group)
+    {
+        if (!DataSet.ItemClasses.Contains(itemClass))
+            ThrowHelper.ThrowArgumentException($"Item class \"{itemClass}\" not found in dataset \"{DataSet}\"");
+        if (!ItemClassesGroups.Contains(group))
+            ThrowHelper.ThrowArgumentException($"Group \"{group}\" not found");
+        group.RemoveItemClass(itemClass);
+    }
+
+    public void DeleteGroup(ItemClassesGroup group) => Guard.IsTrue(_itemClassesGroups.Remove(group));
+
+    private DataSet _dataSet;
+    private readonly List<ItemClassesGroup> _itemClassesGroups;
+
+    private Profile()
+    {
+        Name = null!;
+        Description = null!;
+        _dataSet = null!;
+        _itemClassesGroups = null!;
+    }
+
+    private void ClearGroups()
+    {
+        foreach (var group in ItemClassesGroups)
+            group.Clear();
+    }
+}
