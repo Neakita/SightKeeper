96d976215bf4554f61fe890800765a297fadb5d0
KatterMaw <narcat99@gmail.com>
2023-10-20 13:02:48 +0500

    Check velocities list capacity in debug

--- SightKeeper.Services/Prediction/Handling/MouseMoving/Decorators/Preemption/StabilizedPreemptionComputer.cs
+++ SightKeeper.Services/Prediction/Handling/MouseMoving/Decorators/Preemption/StabilizedPreemptionComputer.cs
@@ -12,6 +12,9 @@ public sealed class StabilizedPreemptionComputer : PreemptionComputer
 	{
 		Guard.IsNotNull(profile.PreemptionSettings?.StabilizationSettings);
 		_preemptionFactor = new Vector2(profile.PreemptionSettings.HorizontalFactor, profile.PreemptionSettings.VerticalFactor);
+#if DEBUG
+		_bufferSize = profile.PreemptionSettings.StabilizationSettings.BufferSize;
+#endif
 		_velocities = new List<Vector2>(profile.PreemptionSettings.StabilizationSettings.BufferSize);
 		_method = profile.PreemptionSettings.StabilizationSettings.Method switch
 		{
@@ -23,9 +26,12 @@ public sealed class StabilizedPreemptionComputer : PreemptionComputer
 	
 	public Vector2 ComputePreemption(Vector2 moveVector, TimeSpan timeDelta)
 	{
+#if DEBUG
+		Guard.IsLessThanOrEqualTo(_velocities.Capacity, _bufferSize);
+#endif
 		var targetVelocity = moveVector + _previousPreemption;
 		targetVelocity /= (float)timeDelta.TotalMilliseconds;
-		
+
 		if (_velocities.Count == _velocities.Capacity)
 			_velocities.RemoveAt(0);
 		_velocities.Add(targetVelocity);
@@ -47,6 +53,9 @@ public sealed class StabilizedPreemptionComputer : PreemptionComputer
 	
 	private const float BasePreemptionFactor = 100;
 	private readonly Vector2 _preemptionFactor;
+#if DEBUG
+	private readonly byte _bufferSize;
+#endif
 	private readonly List<Vector2> _velocities;
 	private readonly Func<IEnumerable<float>, float> _method;
 	private Vector2 _previousPreemption = Vector2.Zero;
