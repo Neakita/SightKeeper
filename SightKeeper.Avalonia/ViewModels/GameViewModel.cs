594546c243bc1b3bd3e7a6fefcf17310c1a2089a
KatterMaw <narcat99@gmail.com>
2024-03-25 14:54:56 +0500

    Add GamesRepositoryViewModel and GameViewModel

---
+++ SightKeeper.Avalonia/ViewModels/GameViewModel.cs
@@ -0,0 +1,38 @@
+﻿using SightKeeper.Domain.Model;
+
+namespace SightKeeper.Avalonia.ViewModels;
+
+internal sealed class GameViewModel : ViewModel
+{
+	public string Title
+	{
+		get => _game.Title;
+		set
+		{
+			SetProperty(_game.Title, value, title => _game.Title = title);
+		}
+	}
+	public string ProcessName
+	{
+		get => _game.ProcessName;
+		set
+		{
+			SetProperty(_game.ProcessName, value, processName => _game.ProcessName = processName);
+		}
+	}
+	public string? ExecutablePath
+	{
+		get => _game.ExecutablePath;
+		set
+		{
+			SetProperty(_game.ExecutablePath, value, executablePath => _game.ExecutablePath = executablePath);
+		}
+	}
+
+	public GameViewModel(Game game)
+	{
+		_game = game;
+	}
+
+	private readonly Game _game;
+}
---
+++ SightKeeper.Avalonia/ViewModels/GamesRepositoryViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using DynamicData;
+using SightKeeper.Application;
+using SightKeeper.Domain.Model;
+
+namespace SightKeeper.Avalonia.ViewModels;
+
+internal sealed class GamesRepositoryViewModel : IDisposable
+{
+	public ReadOnlyObservableCollection<GameViewModel> Games { get; }
+	
+	public GamesRepositoryViewModel(GamesDataAccess gamesDataAccess)
+	{
+		_games.AddRange(gamesDataAccess.Games);
+		_disposable = _games
+			.Connect()
+			.Transform(game => new GameViewModel(game))
+			.Bind(out var games)
+			.Subscribe();
+		Games = games;
+	}
+
+	public void Dispose()
+	{
+		_disposable.Dispose();
+		_games.Dispose();
+	}
+
+	private readonly IDisposable _disposable;
+	private readonly SourceList<Game> _games = new();
+}
