5829990c67c0a3da587bf8dc1c1b9ee6f2629c64
Katter <narcat99@gmail.com>
2024-05-27 06:54:02 +0500

    BytesToBitmapConverter not nullable returns

--- SightKeeper.Avalonia/Misc/Converters/BytesToBitmapConverter.cs
+++ SightKeeper.Avalonia/Misc/Converters/BytesToBitmapConverter.cs
@@ -9,7 +9,7 @@ namespace SightKeeper.Avalonia.Misc.Converters;
 
 public sealed class BytesToBitmapConverter : IValueConverter
 {
-	public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
 	{
 		if (value is not byte[] bytes || !targetType.IsAssignableFrom(typeof(Bitmap))) // converter used for the wrong type
 			return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
@@ -17,6 +17,6 @@ public sealed class BytesToBitmapConverter : IValueConverter
 		return new Bitmap(stream);
 	}
 
-	public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+	public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
 		throw new NotSupportedException();
 }
