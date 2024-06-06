6e6c4a1257662bbc805b3bf06d8d0a7cafd0a000
Katter <narcat99@gmail.com>
2024-06-06 14:02:16 +0500

    Create AssetsLibrary and make DetectorAssetsLibrary inherit it

---
+++ SightKeeper.Domain/Model/DataSets/AssetsLibrary.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using CommunityToolkit.Diagnostics;
+
+namespace SightKeeper.Domain.Model.DataSets;
+
+public abstract class AssetsLibrary<TAsset> : IReadOnlyCollection<TAsset> where TAsset : Asset
+{
+	public int Count => _assets.Count;
+
+	public IEnumerator<TAsset> GetEnumerator()
+	{
+		return _assets.GetEnumerator();
+	}
+
+	public void DeleteAsset(TAsset asset)
+	{
+		var isRemoved = _assets.Remove(asset);
+		Guard.IsTrue(isRemoved);
+	}
+
+	protected void AddAsset(TAsset asset)
+	{
+		var isAdded = _assets.Add(asset);
+		Guard.IsTrue(isAdded);
+	}
+
+	private readonly HashSet<TAsset> _assets = new();
+
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+		return GetEnumerator();
+	}
+}
--- SightKeeper.Domain/Model/DataSets/Detector/DetectorAssetsLibrary.cs
+++ SightKeeper.Domain/Model/DataSets/Detector/DetectorAssetsLibrary.cs
@@ -1,41 +1,11 @@
-﻿using System.Collections;
-using CommunityToolkit.Diagnostics;
+﻿namespace SightKeeper.Domain.Model.DataSets.Detector;
 
-namespace SightKeeper.Domain.Model.DataSets.Detector;
-
-public sealed class DetectorAssetsLibrary : IReadOnlyCollection<DetectorAsset>
+public sealed class DetectorAssetsLibrary : AssetsLibrary<DetectorAsset>
 {
-	public int Count => _assets.Count;
-
-	public IEnumerator<DetectorAsset> GetEnumerator()
-	{
-		return _assets.GetEnumerator();
-	}
-
 	public DetectorAsset MakeAsset(Screenshot screenshot)
 	{
-		// TODO
-		/*Guard.IsNull(screenshot.Asset);
-		screenshot.Asset = new Asset(screenshot);*/
 		DetectorAsset asset = new(screenshot);
-		bool isAdded = _assets.Add(asset);
-		Guard.IsTrue(isAdded);
+		AddAsset(asset);
 		return asset;
-	}
-
-	public void DeleteAsset(DetectorAsset asset)
-	{
-		// TODO
-		var isRemoved = _assets.Remove(asset);
-		Guard.IsTrue(isRemoved);
-		asset.ClearItems();
-		/*asset.Screenshot.Asset = null;*/
-	}
-
-	private readonly HashSet<DetectorAsset> _assets = new();
-
-	IEnumerator IEnumerable.GetEnumerator()
-	{
-		return GetEnumerator();
 	}
 }
