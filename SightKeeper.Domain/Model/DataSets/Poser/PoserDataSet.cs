20ecb82a11373a6a0b9618076c5cd10cb3cb11f0
Katter <narcat99@gmail.com>
2024-06-17 02:29:50 +0500

    Add missing PoserDataSet.Weights

--- SightKeeper.Domain/Model/DataSets/Poser/PoserDataSet.cs
+++ SightKeeper.Domain/Model/DataSets/Poser/PoserDataSet.cs
@@ -4,10 +4,12 @@ public sealed class PoserDataSet : DataSet
 {
 	public PoserTagsLibrary Tags { get; }
 	public PoserAssetsLibrary Assets { get; }
+	public PoserWeightsLibrary Weights { get; }
 
 	public PoserDataSet(string name, ushort resolution) : base(name, resolution)
 	{
 		Tags = new PoserTagsLibrary(this);
 		Assets = new PoserAssetsLibrary(this);
+		Weights = new PoserWeightsLibrary(this);
 	}
 }
