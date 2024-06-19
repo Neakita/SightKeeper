06625b94b9ab3a17bcf491fe7c66d8680ffd9b8f
Katter <narcat99@gmail.com>
2024-06-19 12:59:22 +0500

    ScreenshotsLibrary some docs and possibly optimize ClearExceed method

--- SightKeeper.Domain/Model/DataSets/ScreenshotsLibrary.cs
+++ SightKeeper.Domain/Model/DataSets/ScreenshotsLibrary.cs
@@ -6,7 +6,16 @@ namespace SightKeeper.Domain.Model.DataSets;
 
 public sealed class ScreenshotsLibrary : IReadOnlyCollection<Screenshot>
 {
+	/// <summary>
+	/// The maximum number of screenshots that can be contained in this library.
+	/// If not specified (null), an unlimited number of images can be stored.
+	/// </summary>
     public ushort? MaxQuantity { get; set; }
+	
+    /// <summary>
+    /// The current number of screenshots stored.
+    /// If the <see cref="MaxQuantity"/> is specified, it (generally) cannot exceed it.
+    /// </summary>
     public int Count => _screenshots.Count;
 
     public void DeleteScreenshot(Screenshot screenshot)
@@ -31,10 +40,15 @@ public sealed class ScreenshotsLibrary : IReadOnlyCollection<Screenshot>
 	    var exceedAmount = _screenshots.Count - MaxQuantity.Value;
 	    if (exceedAmount <= 0)
 		    return ImmutableArray<Screenshot>.Empty;
-	    var screenshotsToDelete = _screenshots.Take(exceedAmount).ToImmutableArray();
-	    foreach (var screenshot in screenshotsToDelete)
-		    _screenshots.Remove(screenshot);
-	    return screenshotsToDelete;
+	    var builder = ImmutableArray.CreateBuilder<Screenshot>(exceedAmount);
+	    for (int i = 0; i < exceedAmount; i++)
+	    {
+		    var oldestScreenshot = _screenshots.Min;
+		    Guard.IsNotNull(oldestScreenshot);
+		    Guard.IsTrue(_screenshots.Remove(oldestScreenshot));
+		    builder.Add(oldestScreenshot);
+	    }
+	    return builder.ToImmutable();
     }
 
     private readonly SortedSet<Screenshot> _screenshots = new(ScreenshotsDateComparer.Instance);
