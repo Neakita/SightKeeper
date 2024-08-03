0f430a4ddb4843db5519f275d734c305a8576d8a
Katter <narcat99@gmail.com>
2024-08-03 20:00:47 +0500

    AppearanceSettingsViewModel ObservableProperty

--- SightKeeper.Avalonia/Settings/Appearance/AppearanceSettingsViewModel.cs
+++ SightKeeper.Avalonia/Settings/Appearance/AppearanceSettingsViewModel.cs
@@ -1,28 +1,23 @@
 ﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using SightKeeper.Avalonia.Extensions;
 using SightKeeper.Avalonia.ViewModels;
 
 namespace SightKeeper.Avalonia.Settings.Appearance;
 
 [Browsable(true)]
-internal sealed class AppearanceSettingsViewModel : ViewModel, SettingsSection
+internal sealed partial class AppearanceSettingsViewModel : ViewModel, SettingsSection
 {
 	public string Header => "Appearance";
 
+	[ObservableProperty]
 	[Description("Custom window decorations look like the rest of the window and integrate with the contents, taking up less space. Off by default, using system decorations, providing a native experience.")]
-	public bool CustomDecorations
-	{
-		get => _customDecorations;
-		set
-		{
-			if (value == _customDecorations)
-				return;
-			_customDecorations = value;
-			OnPropertyChanged();
-			// ISSUE Should not directly reference window
-			ControlExtensions.ReopenWindow<MainWindow>();
-		}
-	}
-
 	private bool _customDecorations;
+
+	partial void OnCustomDecorationsChanged(bool value)
+	{
+		_ = value;
+		// ISSUE Should not directly reference window
+		ControlExtensions.ReopenWindow<MainWindow>();
+	}
 }
