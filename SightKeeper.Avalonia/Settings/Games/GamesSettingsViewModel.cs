13dfbc078580ac99235f4acf3dc820d0663c1cbe
Katter <narcat99@gmail.com>
2024-08-10 08:58:45 +0500

    Get rid of ExistingGameViewModel

--- SightKeeper.Avalonia/Settings/Games/GamesSettingsViewModel.cs
+++ SightKeeper.Avalonia/Settings/Games/GamesSettingsViewModel.cs
@@ -2,9 +2,12 @@
 using System.Threading.Tasks;
 using Autofac;
 using CommunityToolkit.Mvvm.Input;
+using Material.Icons;
 using SightKeeper.Application.Games;
 using SightKeeper.Avalonia.Dialogs;
+using SightKeeper.Avalonia.Dialogs.MessageBox;
 using SightKeeper.Avalonia.ViewModels;
+using SightKeeper.Domain.Model;
 
 namespace SightKeeper.Avalonia.Settings.Games;
 
@@ -17,17 +20,20 @@ internal sealed partial class GamesSettingsViewModel : ViewModel, SettingsSectio
 		IComponentContext context,
 		GamesRepositoryViewModel gamesRepository,
 		DialogManager dialogManager,
-		GameCreator gameCreator)
+		GameCreator gameCreator,
+		GamesDataAccess gamesDataAccess)
 	{
 		_context = context;
 		_dialogManager = dialogManager;
 		_gameCreator = gameCreator;
+		_gamesDataAccess = gamesDataAccess;
 		Games = gamesRepository.Games;
 	}
 
 	private readonly IComponentContext _context;
 	private readonly DialogManager _dialogManager;
 	private readonly GameCreator _gameCreator;
+	private readonly GamesDataAccess _gamesDataAccess;
 
 	[RelayCommand]
 	private async Task AddGame()
@@ -37,5 +43,19 @@ internal sealed partial class GamesSettingsViewModel : ViewModel, SettingsSectio
 		var isApplied = await _dialogManager.ShowDialogAsync(viewModel);
 		if (isApplied)
 			_gameCreator.CreateGame(viewModel);
+	}
+
+	[RelayCommand]
+	private async Task DeleteGame(Game game)
+	{
+		MessageBoxButtonDefinition confirmButton = new("Delete", MaterialIconKind.TrashCanOutline);
+		MessageBoxButtonDefinition cancelButton = new("Cancel", MaterialIconKind.Close, true);
+		MessageBoxDialogViewModel messageBox = new(
+			"Game deletion confirmation",
+			$"Are you sure you want to delete \"{game.Title}\" from the list of registered games?",
+			confirmButton, cancelButton);
+		var messageBoxResult = await _dialogManager.ShowDialogAsync(messageBox);
+		if (messageBoxResult == confirmButton)
+			_gamesDataAccess.RemoveGame(game);
 	}
 }
--- SightKeeper.Avalonia/ViewModels/GamesRepositoryViewModel.cs
+++ SightKeeper.Avalonia/ViewModels/GamesRepositoryViewModel.cs
@@ -5,26 +5,24 @@ using CommunityToolkit.Diagnostics;
 using DynamicData;
 using SightKeeper.Application.Extensions;
 using SightKeeper.Application.Games;
-using SightKeeper.Avalonia.Dialogs;
 using SightKeeper.Domain.Model;
 
 namespace SightKeeper.Avalonia.ViewModels;
 
 internal sealed class GamesRepositoryViewModel : IDisposable
 {
-	public ReadOnlyObservableCollection<ExistingGameViewModel> Games { get; }
+	public ReadOnlyObservableCollection<GameViewModel> Games { get; }
 	
 	public GamesRepositoryViewModel(
 		GamesDataAccess gamesDataAccess,
 		GameIconProvider gameIconProvider,
-		GameExecutableDisplayer gameExecutableDisplayer,
-		DialogManager dialogManager)
+		GameExecutableDisplayer gameExecutableDisplayer)
 	{
 		_games.AddRange(gamesDataAccess.Games);
 		gamesDataAccess.GameAdded.Subscribe(OnGameAdded).DisposeWith(_constructorDisposables);
 		gamesDataAccess.GameRemoved.Subscribe(OnGameRemoved).DisposeWith(_constructorDisposables);
 		_games.Connect()
-			.Transform(game => new ExistingGameViewModel(game, gameIconProvider, gameExecutableDisplayer, dialogManager, gamesDataAccess))
+			.Transform(game => new GameViewModel(game, gameIconProvider, gameExecutableDisplayer))
 			.Bind(out var games)
 			.Subscribe()
 			.DisposeWith(_constructorDisposables);
