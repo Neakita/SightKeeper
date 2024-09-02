6da6814f00243eb5c7024bf506fb5bdd123509aa
Katter <narcat99@gmail.com>
2024-09-02 13:27:42 +0500

    Remove unused extension method in domain

--- SightKeeper.Domain/Extensions.cs
+++ SightKeeper.Domain/Extensions.cs
@@ -5,12 +5,6 @@ namespace SightKeeper.Domain;
 
 internal static class Extensions
 {
-	public static bool HasDuplicates<T>(this IEnumerable<T> enumerable)
-	{
-		HashSet<T> hashSet = new();
-		return !enumerable.All(hashSet.Add);
-	}
-
 	public static ImmutableHashSet<T> ToImmutableHashSetThrowOnDuplicate<T>(this IEnumerable<T> enumerable)
 	{
 		var tagsBuilder = ImmutableHashSet.CreateBuilder<T>();
