ab3c1dd6e57d3d4b16c5d345f373f5813e3ba233
Katter <narcat99@gmail.com>
2024-09-02 13:15:16 +0500

    Remove unused converters

--- SightKeeper.Avalonia/Converters.cs
+++ SightKeeper.Avalonia/Converters.cs
@@ -1,7 +1,6 @@
 ﻿using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Data.Converters;
-using SightKeeper.Avalonia.Misc.Converters;
 
 namespace SightKeeper.Avalonia;
 
@@ -9,12 +8,6 @@ internal static class Converters
 {
 	public static FuncValueConverter<object, double> ContentToOpacityConverter { get; } =
 		new(value => value == null ? 0 : 1);
-	public static BytesToBitmapConverter BytesToBitmapConverter { get; } = new();
-	public static ToBrushConverter ToBrushConverter { get; } = new();
-	public static WidthToZIndexConverter WidthToZIndexConverter { get; } = new();
-	public static CanvasNormalizeConverter CanvasNormalizeConverter { get; } = new();
-	public static EnumToDescriptionConverter EnumToDescriptionConverter { get; } = new();
-	public static TagActivationConditionToBoolConverter TagActivationConditionToBoolConverter { get; } = new();
 	public static FuncValueConverter<double, GridLength> DoubleToGridLengthConvert { get; } =
 		new(d => new GridLength(d));
 
