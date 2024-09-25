9bf5b0f28815aeca5b4a472ad984abbd0bedfd50
Katter <narcat99@gmail.com>
2024-09-25 09:55:04 +0500

    Wrap Screenshoter in ScreenshottingSettingsViewModel properties

--- SightKeeper.Avalonia/Annotation/ScreenshottingOptions/ScreenshottingSettingsViewModel.cs
+++ SightKeeper.Avalonia/Annotation/ScreenshottingOptions/ScreenshottingSettingsViewModel.cs
@@ -1,55 +1,60 @@
-using CommunityToolkit.Diagnostics;
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using HotKeys.Gestures;
 using SightKeeper.Application;
+using SightKeeper.Avalonia.DataSets;
 
 namespace SightKeeper.Avalonia.Annotation.ScreenshottingOptions;
 
 internal sealed partial class ScreenshottingSettingsViewModel : ViewModel
 {
+	public bool IsEnabled
+	{
+		get => _screenshoter.IsEnabled;
+		set => SetProperty(_screenshoter.IsEnabled, value, _screenshoter, static (screenshoter, newValue) => screenshoter.IsEnabled = newValue);
+	}
+
 	public float FPS
 	{
-		get => _fps;
-		set
-		{
-			Guard.IsGreaterThan(value, 0);
-			SetProperty(ref _fps, value);
-		}
+		get => _screenshoter.FPS;
+		set => SetProperty(_screenshoter.FPS, value, _screenshoter, static (screenshoter, newValue) => screenshoter.FPS = newValue);
 	}
 
 	public ushort ResolutionWidth
 	{
-		get => _resolutionWidth;
-		set
-		{
-			Guard.IsGreaterThanOrEqualTo(value, MinimumResolution);
-			SetProperty(ref _resolutionWidth, value);
-		}
+		get => _screenshoter.Resolution.X;
+		set => SetProperty(_screenshoter.Resolution.X, value, _screenshoter, static (screenshoter, newValue) => screenshoter.Resolution = screenshoter.Resolution.WithX(newValue));
 	}
 
 	public ushort ResolutionHeight
 	{
-		get => _resolutionHeight;
-		set
-		{
-			Guard.IsGreaterThanOrEqualTo(value, MinimumResolution);
-			SetProperty(ref _resolutionHeight, value);
-		}
+		get => _screenshoter.Resolution.Y;
+		set => SetProperty(_screenshoter.Resolution.Y, value, _screenshoter, static (screenshoter, newValue) => screenshoter.Resolution = screenshoter.Resolution.WithY(newValue));
+	}
+
+	public Gesture Gesture
+	{
+		get => _screenshoter.Gesture;
+		set => SetProperty(_screenshoter.Gesture, value, _screenshoter, static (screenshoter, newValue) => screenshoter.Gesture = newValue);
 	}
 
 	public ushort MaximumWidth => _screenBoundsProvider.MainScreenSize.X;
 	public ushort MaximumHeight => _screenBoundsProvider.MainScreenSize.Y;
+	public ReadOnlyObservableCollection<DataSetViewModel> DataSets { get; }
 
-	public ScreenshottingSettingsViewModel(ScreenBoundsProvider screenBoundsProvider)
+	public ScreenshottingSettingsViewModel(ScreenBoundsProvider screenBoundsProvider, Screenshoter screenshoter, DataSetsListViewModel dataSets)
 	{
 		_screenBoundsProvider = screenBoundsProvider;
+		_screenshoter = screenshoter;
+		DataSets = dataSets.DataSets;
 	}
 
-	private const ushort MinimumResolution = 32;
 	private readonly ScreenBoundsProvider _screenBoundsProvider;
-	[ObservableProperty] private bool _isEnabled;
-	[ObservableProperty] private PassiveScalingOptionsViewModel? _scalingOptions;
-	[ObservableProperty] private PassiveWalkingOptionsViewModel? _walkingOptions;
-	private float _fps;
-	private ushort _resolutionWidth;
-	private ushort _resolutionHeight;
+	private readonly Screenshoter _screenshoter;
+	[ObservableProperty] private DataSetViewModel? _selectedDataSet;
+
+	partial void OnSelectedDataSetChanged(DataSetViewModel? value)
+	{
+		_screenshoter.Library = value?.DataSet.ScreenshotsLibrary;
+	}
 }
