937d6c81a21a3bf41157064991a4b9b6de8eb215
Neakita <thekatterdev@gmail.com>
2024-10-24 20:54:30 +0500

    redundant type specification

--- SightKeeper.Avalonia/DataSets/DesignDataSetsViewModel.cs
+++ SightKeeper.Avalonia/DataSets/DesignDataSetsViewModel.cs
@@ -12,10 +12,10 @@ internal sealed class DesignDataSetsViewModel : IDataSetsViewModel
 {
 	public IReadOnlyCollection<DataSetViewModel> DataSets { get; } =
 	[
-		new DataSetViewModel(new ClassifierDataSet { Name = "Classifier DataSet" }),
-		new DataSetViewModel(new DetectorDataSet { Name = "Detector DataSet" }),
-		new DataSetViewModel(new Poser2DDataSet { Name = "Poser 2D DataSet" }),
-		new DataSetViewModel(new Poser3DDataSet { Name = "Poser 3D DataSet" }),
+		new(new ClassifierDataSet { Name = "Classifier DataSet" }),
+		new(new DetectorDataSet { Name = "Detector DataSet" }),
+		new(new Poser2DDataSet { Name = "Poser 2D DataSet" }),
+		new(new Poser3DDataSet { Name = "Poser 3D DataSet" }),
 	];
 
 	public ICommand CreateDataSetCommand { get; } = new RelayCommand(() => { });
