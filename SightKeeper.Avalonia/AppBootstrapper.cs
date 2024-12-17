f4d67a9c78095c5c07f8f6867277af1eecc114fb
Neakita <thekatterdev@gmail.com>
2024-12-17 12:08:14 +0500

    Load data at startup

--- SightKeeper.Avalonia/AppBootstrapper.cs
+++ SightKeeper.Avalonia/AppBootstrapper.cs
@@ -1,9 +1,13 @@
+using MemoryPack;
+
 namespace SightKeeper.Avalonia;
 
 internal static class AppBootstrapper
 {
 	public static void Setup(Composition composition)
 	{
+		MemoryPackFormatterProvider.Register(composition.AppDataFormatter);
+		composition.AppDataAccess.Load();
 		composition.PeriodicAppDataSaver.Start();
 	}
 }
