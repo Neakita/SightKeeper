6c70c98d3ad9a7fc47e917e440450c020abc8af5
Neakita <thekatterdev@gmail.com>
2025-01-13 12:44:15 +0500

    Add DetectorAnnotationContextTests

---
+++ SightKeeper.Avalonia.Tests/Annotation/Contexts/DetectorAnnotationContextTests.cs
@@ -0,0 +1,29 @@
+using FluentAssertions;
+using SightKeeper.Avalonia.Annotation.Contexts;
+using SightKeeper.Avalonia.Annotation.Drawing.Detector;
+using SightKeeper.Avalonia.Annotation.ToolBars;
+
+namespace SightKeeper.Avalonia.Tests.Annotation.Contexts;
+
+public sealed class DetectorAnnotationContextTests
+{
+	[Fact]
+	public void ShouldHaveDetectorToolBar()
+	{
+		Context.ToolBar.Should().BeOfType<DetectorToolBarViewModel>();
+	}
+
+	[Fact]
+	public void ShouldHaveDetectorDrawer()
+	{
+		Context.Drawer.Should().BeOfType<DetectorDrawerViewModel>();
+	}
+
+	[Fact]
+	public void ShouldNotHaveDataSetByDefault()
+	{
+		Context.DataSet.Should().BeNull();
+	}
+
+	private static DetectorAnnotationContext Context => new Composition().DetectorAnnotationContext;
+}
