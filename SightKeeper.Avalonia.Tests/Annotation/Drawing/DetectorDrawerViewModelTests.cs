f3ce6ec3004d03eb7720f710da8963bc9791076c
Neakita <thekatterdev@gmail.com>
2025-01-15 18:00:05 +0500

    DetectorDrawerViewModelTests reduce boilerplate

--- SightKeeper.Avalonia.Tests/Annotation/Drawing/DetectorDrawerViewModelTests.cs
+++ SightKeeper.Avalonia.Tests/Annotation/Drawing/DetectorDrawerViewModelTests.cs
@@ -25,11 +25,11 @@ public sealed class DetectorDrawerViewModelTests
 	public void ShouldHaveItemsFromScreenshotAssociatedAsset()
 	{
 		ScreenshotsLibrary screenshotsLibrary = new();
-		var screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, new Vector2<ushort>(320, 320));
+		var screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, ImageSize);
 		DetectorDataSet dataSet = new();
 		var tag = dataSet.TagsLibrary.CreateTag("TestTag");
 		var asset = dataSet.AssetsLibrary.MakeAsset(screenshot);
-		var item = asset.CreateItem(tag, new Bounding(0.1, 0.1, 0.9, 0.9));
+		var item = asset.CreateItem(tag, Bounding);
 		var drawer = Drawer;
 		drawer.AssetsLibrary = dataSet.AssetsLibrary;
 		drawer.Screenshot = screenshot;
@@ -40,31 +40,29 @@ public sealed class DetectorDrawerViewModelTests
 	public void ShouldCreateItem()
 	{
 		ScreenshotsLibrary screenshotsLibrary = new();
-		var screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, new Vector2<ushort>(320, 320));
+		var screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, ImageSize);
 		DetectorDataSet dataSet = new();
 		var tag = dataSet.TagsLibrary.CreateTag("TestTag");
 		var drawer = Drawer;
 		drawer.AssetsLibrary = dataSet.AssetsLibrary;
 		drawer.Screenshot = screenshot;
 		drawer.SetTag(tag);
-		Bounding bounding = new(0.1, 0.1, 0.9, 0.9);
-		drawer.CreateItemCommand.Execute(bounding);
-		drawer.Items.Should().Contain(itemViewModel => itemViewModel.Bounding == bounding);
-		dataSet.AssetsLibrary.Assets.Single().Value.Items.Should().Contain(item => item.Bounding == bounding);
+		drawer.CreateItemCommand.Execute(Bounding);
+		drawer.Items.Should().Contain(itemViewModel => itemViewModel.Bounding == Bounding);
+		dataSet.AssetsLibrary.Assets.Single().Value.Items.Should().Contain(item => item.Bounding == Bounding);
 	}
 
 	[Fact]
 	public void ShouldNotCreateItemWithoutAssetsLibrary()
 	{
 		ScreenshotsLibrary screenshotsLibrary = new();
-		var screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, new Vector2<ushort>(320, 320));
+		var screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, ImageSize);
 		DetectorDataSet dataSet = new();
 		var tag = dataSet.TagsLibrary.CreateTag("TestTag");
 		var drawer = Drawer;
 		drawer.Screenshot = screenshot;
 		drawer.SetTag(tag);
-		Bounding bounding = new(0.1, 0.1, 0.9, 0.9);
-		Assert.ThrowsAny<Exception>(() => drawer.CreateItemCommand.Execute(bounding));
+		Assert.ThrowsAny<Exception>(() => drawer.CreateItemCommand.Execute(Bounding));
 	}
 
 	[Fact]
@@ -75,32 +73,30 @@ public sealed class DetectorDrawerViewModelTests
 		var drawer = Drawer;
 		drawer.AssetsLibrary = dataSet.AssetsLibrary;
 		drawer.SetTag(tag);
-		Bounding bounding = new(0.1, 0.1, 0.9, 0.9);
-		Assert.ThrowsAny<Exception>(() => drawer.CreateItemCommand.Execute(bounding));
+		Assert.ThrowsAny<Exception>(() => drawer.CreateItemCommand.Execute(Bounding));
 	}
 
 	[Fact]
 	public void ShouldNotCreateItemWithoutTag()
 	{
 		ScreenshotsLibrary screenshotsLibrary = new();
-		var screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, new Vector2<ushort>(320, 320));
+		var screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, ImageSize);
 		DetectorDataSet dataSet = new();
 		var drawer = Drawer;
 		drawer.AssetsLibrary = dataSet.AssetsLibrary;
 		drawer.Screenshot = screenshot;
-		Bounding bounding = new(0.1, 0.1, 0.9, 0.9);
-		Assert.ThrowsAny<Exception>(() => drawer.CreateItemCommand.Execute(bounding));
+		Assert.ThrowsAny<Exception>(() => drawer.CreateItemCommand.Execute(Bounding));
 	}
 
 	[Fact]
 	public void ShouldClearItemsAfterChangingAssetsLibrary()
 	{
 		ScreenshotsLibrary screenshotsLibrary = new();
-		var screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, new Vector2<ushort>(320, 320));
+		var screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, ImageSize);
 		DetectorDataSet dataSet = new();
 		var tag = dataSet.TagsLibrary.CreateTag("TestTag");
 		var asset = dataSet.AssetsLibrary.MakeAsset(screenshot);
-		asset.CreateItem(tag, new Bounding(0.1, 0.1, 0.9, 0.9));
+		asset.CreateItem(tag, Bounding);
 		var drawer = Drawer;
 		drawer.AssetsLibrary = dataSet.AssetsLibrary;
 		drawer.Screenshot = screenshot;
@@ -112,17 +108,19 @@ public sealed class DetectorDrawerViewModelTests
 	public void ShouldClearItemsAfterChangingScreenshot()
 	{
 		ScreenshotsLibrary screenshotsLibrary = new();
-		var screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, new Vector2<ushort>(320, 320));
+		var screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, ImageSize);
 		DetectorDataSet dataSet = new();
 		var tag = dataSet.TagsLibrary.CreateTag("TestTag");
 		var asset = dataSet.AssetsLibrary.MakeAsset(screenshot);
-		asset.CreateItem(tag, new Bounding(0.1, 0.1, 0.9, 0.9));
+		asset.CreateItem(tag, Bounding);
 		var drawer = Drawer;
 		drawer.AssetsLibrary = dataSet.AssetsLibrary;
 		drawer.Screenshot = screenshot;
-		drawer.Screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, new Vector2<ushort>(320, 320));
+		drawer.Screenshot = screenshotsLibrary.CreateScreenshot(DateTimeOffset.UtcNow, ImageSize);
 		drawer.Items.Should().BeEmpty();
 	}
 
+	private static Vector2<ushort> ImageSize => new(320, 320);
+	private static Bounding Bounding => new(0.1, 0.1, 0.9, 0.9);
 	private static DetectorDrawerViewModel Drawer => new Composition().DetectorAnnotationContext.Drawer;
 }
