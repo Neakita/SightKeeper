8af61d53b74715a1d35f324532676460f0580322
Neakita <thekatterdev@gmail.com>
2025-01-18 12:23:27 +0500

    Move ItemsAsset<TItem> from ItemsAsset.cs to it's own file ItemsAsset`1.cs

--- SightKeeper.Domain/DataSets/Assets/ItemsAsset.cs
+++ SightKeeper.Domain/DataSets/Assets/ItemsAsset.cs
@@ -4,27 +4,3 @@ public abstract class ItemsAsset : Asset
 {
 	public abstract IReadOnlyCollection<AssetItem> Items { get; }
 }
-
-public abstract class ItemsAsset<TItem> : ItemsAsset where TItem : AssetItem
-{
-	public override IReadOnlyCollection<TItem> Items => _items.AsReadOnly();
-
-	public void DeleteItem(TItem item)
-	{
-		var isRemoved = _items.Remove(item);
-		if (!isRemoved)
-			throw new ArgumentException("Specified item was not found and therefore not deleted", nameof(item));
-	}
-
-	public void ClearItems()
-	{
-		_items.Clear();
-	}
-
-	protected void AddItem(TItem item)
-	{
-		_items.Add(item);
-	}
-
-	private readonly List<TItem> _items = new();
-}
---
+++ SightKeeper.Domain/DataSets/Assets/ItemsAsset`1.cs
@@ -0,0 +1,25 @@
+namespace SightKeeper.Domain.DataSets.Assets;
+
+public abstract class ItemsAsset<TItem> : ItemsAsset where TItem : AssetItem
+{
+	public override IReadOnlyCollection<TItem> Items => _items.AsReadOnly();
+
+	public void DeleteItem(TItem item)
+	{
+		var isRemoved = _items.Remove(item);
+		if (!isRemoved)
+			throw new ArgumentException("Specified item was not found and therefore not deleted", nameof(item));
+	}
+
+	public void ClearItems()
+	{
+		_items.Clear();
+	}
+
+	protected void AddItem(TItem item)
+	{
+		_items.Add(item);
+	}
+
+	private readonly List<TItem> _items = new();
+}
