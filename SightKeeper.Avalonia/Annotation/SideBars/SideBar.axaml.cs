39291b22d30f0d64ab92d65cdcd7acd266f8b496
Neakita <thekatterdev@gmail.com>
2025-01-20 07:44:24 +0500

    Move AnnotationTab Grid Column 2 content to new SideBar view

---
+++ SightKeeper.Avalonia/Annotation/SideBars/SideBar.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SightKeeper.Avalonia.Annotation.SideBars;
+
+public sealed partial class SideBar : UserControl
+{
+	public SideBar()
+	{
+		InitializeComponent();
+	}
+}
