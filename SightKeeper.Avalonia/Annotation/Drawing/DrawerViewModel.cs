8f4f07d9b57329e3293c445d675febbbcf8961ab
Neakita <thekatterdev@gmail.com>
2025-01-20 07:39:04 +0500

    Move AnnotationTab Grid Column 2 content to Drawer view reducing AnnotationTab volume for better readability

--- SightKeeper.Avalonia/Annotation/Drawing/DrawerViewModel.cs
+++ SightKeeper.Avalonia/Annotation/Drawing/DrawerViewModel.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using SightKeeper.Domain.DataSets.Assets;
 using SightKeeper.Domain.DataSets.Tags;
+using SightKeeper.Domain.Screenshots;
 
 namespace SightKeeper.Avalonia.Annotation.Drawing;
 
 public abstract partial class DrawerViewModel : ViewModel
 {
+	[ObservableProperty] public partial Screenshot? Screenshot { get; set; }
 	public abstract IReadOnlyCollection<DrawerItemViewModel> Items { get; }
 	public abstract Tag? Tag { get; }
 
