a68b741daa3b7843fac823a3fa3ec4a6113e9aa7
Neakita <thekatterdev@gmail.com>
2025-01-26 09:40:34 +0500

    Move TagsLibrary<TTag> to its own file

--- SightKeeper.Domain/DataSets/Tags/TagsLibrary.cs
+++ SightKeeper.Domain/DataSets/Tags/TagsLibrary.cs
@@ -6,35 +6,3 @@ public abstract class TagsLibrary : TagsOwner
 
 	public abstract Tag CreateTag(string name);
 }
-
-public sealed class TagsLibrary<TTag> : TagsLibrary where TTag : Tag
-{
-	public override IReadOnlyList<TTag> Tags => _tags.AsReadOnly();
-
-	public override TTag CreateTag(string name)
-	{
-		var tag = _tagsFactory.CreateTag(this, name);
-		_tags.Add(tag);
-		return tag;
-	}
-
-	public void DeleteTag(TTag tag)
-	{
-		bool isTagInUse = _tagsUsageProvider.IsInUse(tag);
-		if (isTagInUse)
-			TagIsInUseException.ThrowForDeletion(tag);
-		var isRemoved = _tags.Remove(tag);
-		if (!isRemoved)
-			throw new ArgumentException("Specified tag was not found and therefore not deleted", nameof(tag));
-	}
-
-	internal TagsLibrary(TagsFactory<TTag> tagsFactory, TagsUsageProvider tagsUsageProvider)
-	{
-		_tagsFactory = tagsFactory;
-		_tagsUsageProvider = tagsUsageProvider;
-	}
-
-	private readonly TagsFactory<TTag> _tagsFactory;
-	private readonly TagsUsageProvider _tagsUsageProvider;
-	private readonly List<TTag> _tags = new();
-}
---
+++ SightKeeper.Domain/DataSets/Tags/TagsLibrary`1.cs
@@ -0,0 +1,33 @@
+namespace SightKeeper.Domain.DataSets.Tags;
+
+public sealed class TagsLibrary<TTag> : TagsLibrary where TTag : Tag
+{
+	public override IReadOnlyList<TTag> Tags => _tags.AsReadOnly();
+
+	public override TTag CreateTag(string name)
+	{
+		var tag = _tagsFactory.CreateTag(this, name);
+		_tags.Add(tag);
+		return tag;
+	}
+
+	public void DeleteTag(TTag tag)
+	{
+		bool isTagInUse = _tagsUsageProvider.IsInUse(tag);
+		if (isTagInUse)
+			TagIsInUseException.ThrowForDeletion(tag);
+		var isRemoved = _tags.Remove(tag);
+		if (!isRemoved)
+			throw new ArgumentException("Specified tag was not found and therefore not deleted", nameof(tag));
+	}
+
+	internal TagsLibrary(TagsFactory<TTag> tagsFactory, TagsUsageProvider tagsUsageProvider)
+	{
+		_tagsFactory = tagsFactory;
+		_tagsUsageProvider = tagsUsageProvider;
+	}
+
+	private readonly TagsFactory<TTag> _tagsFactory;
+	private readonly TagsUsageProvider _tagsUsageProvider;
+	private readonly List<TTag> _tags = new();
+}
