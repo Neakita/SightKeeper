1265d49ffe9cfaa05bd61b3e580385c311ade7bc
Neakita <thekatterdev@gmail.com>
2025-01-30 23:08:24 +0500

    Make smaller items stay in front of the larger items

---
+++ SightKeeper.Avalonia/Annotation/Drawing/ReverseBoundingAreaToIntConverter.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using CommunityToolkit.Diagnostics;
+using SightKeeper.Domain.DataSets.Assets;
+
+namespace SightKeeper.Avalonia.Annotation.Drawing;
+
+internal sealed class ReverseBoundingAreaToIntConverter : IValueConverter
+{
+	public static ReverseBoundingAreaToIntConverter Instance { get; } = new();
+
+	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		if (value is not Bounding bounding)
+			return AvaloniaProperty.UnsetValue;
+		Guard.IsBetweenOrEqualTo(bounding.Width, 0, 1);
+		Guard.IsBetweenOrEqualTo(bounding.Height, 0, 1);
+		var area = bounding.Width * bounding.Height;
+		return MaxValue * (1 - area);
+	}
+
+	public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		throw new NotSupportedException();
+	}
+
+	private const int MaxValue = int.MaxValue;
+}
