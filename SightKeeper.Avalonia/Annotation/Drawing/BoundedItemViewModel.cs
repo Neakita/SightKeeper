10cbe10b25c3e10edad2e3418ef1f6bb89e7af2f
Neakita <thekatterdev@gmail.com>
2025-02-18 19:49:43 +0500

    Add DrawerItemDataContext which only holds Position

--- SightKeeper.Avalonia/Annotation/Drawing/BoundedItemViewModel.cs
+++ SightKeeper.Avalonia/Annotation/Drawing/BoundedItemViewModel.cs
@@ -1,10 +1,11 @@
 using SightKeeper.Application.Annotation;
+using SightKeeper.Domain;
 using SightKeeper.Domain.DataSets.Assets;
 using SightKeeper.Domain.DataSets.Tags;
 
 namespace SightKeeper.Avalonia.Annotation.Drawing;
 
-public abstract class BoundedItemViewModel : ViewModel, BoundedItemDataContext
+public abstract class BoundedItemViewModel : ViewModel, BoundedItemDataContext, DrawerItemDataContext
 {
 	public abstract Tag Tag { get; }
 	public abstract BoundedItem Item { get; }
@@ -12,9 +13,15 @@ public abstract class BoundedItemViewModel : ViewModel, BoundedItemDataContext
 	public Bounding Bounding
 	{
 		get => Item.Bounding;
-		set => SetProperty(Bounding, value, this,
-			(viewModel, bounding) => viewModel._boundingEditor.SetBounding(viewModel.Item, bounding));
+		set
+		{
+			if (SetProperty(Bounding, value, this,
+				(viewModel, bounding) => viewModel._boundingEditor.SetBounding(viewModel.Item, bounding)))
+				OnPropertyChanged(nameof(Position));
+		}
 	}
+
+	public Vector2<double> Position => Bounding.Position;
 
 	protected BoundedItemViewModel(BoundingEditor boundingEditor)
 	{
---
+++ SightKeeper.Avalonia/Annotation/Drawing/DrawerItemDataContext.cs
@@ -0,0 +1,8 @@
+using SightKeeper.Domain;
+
+namespace SightKeeper.Avalonia.Annotation.Drawing;
+
+public interface DrawerItemDataContext
+{
+	Vector2<double> Position { get; }
+}
