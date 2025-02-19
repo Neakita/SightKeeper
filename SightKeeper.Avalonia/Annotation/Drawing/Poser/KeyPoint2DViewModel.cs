ce6bec2fc5722523430d2b112fb4993a9fe1519d
Neakita <thekatterdev@gmail.com>
2025-02-19 05:44:11 +0500

    Add KeyPoint2DViewModel

---
+++ SightKeeper.Avalonia/Annotation/Drawing/Poser/KeyPoint2DViewModel.cs
@@ -0,0 +1,18 @@
+using SightKeeper.Application.Annotation;
+using SightKeeper.Domain.DataSets.Poser;
+using SightKeeper.Domain.DataSets.Tags;
+
+namespace SightKeeper.Avalonia.Annotation.Drawing.Poser;
+
+internal sealed class KeyPoint2DViewModel : KeyPointViewModel
+{
+	public override Poser2DItemViewModel Item { get; }
+	public override KeyPoint Value { get; }
+	public override Tag Tag => Value.Tag;
+
+	public KeyPoint2DViewModel(PoserAnnotator annotator, Poser2DItemViewModel item, KeyPoint value) : base(annotator)
+	{
+		Item = item;
+		Value = value;
+	}
+}
