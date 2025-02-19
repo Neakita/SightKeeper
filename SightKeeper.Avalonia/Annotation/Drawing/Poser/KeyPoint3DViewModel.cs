d740450fe3164e5c29e8754398a06bb810b76922
Neakita <thekatterdev@gmail.com>
2025-02-19 05:39:46 +0500

    KeyPointViewModel.Position implement in place instead of abstraction

--- SightKeeper.Avalonia/Annotation/Drawing/Poser/KeyPoint3DViewModel.cs
+++ SightKeeper.Avalonia/Annotation/Drawing/Poser/KeyPoint3DViewModel.cs
@@ -1,18 +1,17 @@
 using System;
-using SightKeeper.Domain;
+using SightKeeper.Application.Annotation;
+using SightKeeper.Domain.DataSets.Poser;
 using SightKeeper.Domain.DataSets.Tags;
 
 namespace SightKeeper.Avalonia.Annotation.Drawing.Poser;
 
 public sealed class KeyPoint3DViewModel : KeyPointViewModel
 {
+	public override KeyPoint Value => throw new NotImplementedException();
 	public override Tag Tag => throw new NotImplementedException();
+	public override Poser3DItemViewModel Item => throw new NotImplementedException();
 
-	public override PoserItemViewModel Item => throw new NotImplementedException();
-
-	public override Vector2<double> Position
+	public KeyPoint3DViewModel(PoserAnnotator annotator) : base(annotator)
 	{
-		get => throw new NotImplementedException();
-		set => throw new NotImplementedException();
 	}
 }
--- SightKeeper.Avalonia/Annotation/Drawing/Poser/KeyPointViewModel.cs
+++ SightKeeper.Avalonia/Annotation/Drawing/Poser/KeyPointViewModel.cs
@@ -1,11 +1,33 @@
+using SightKeeper.Application.Annotation;
 using SightKeeper.Domain;
+using SightKeeper.Domain.DataSets.Poser;
 using SightKeeper.Domain.DataSets.Tags;
 
 namespace SightKeeper.Avalonia.Annotation.Drawing.Poser;
 
 public abstract class KeyPointViewModel : ViewModel
 {
+	public abstract KeyPoint Value { get; }
 	public abstract Tag Tag { get; }
 	public abstract PoserItemViewModel Item { get; }
-	public abstract Vector2<double> Position { get; set; }
+
+	public Vector2<double> Position
+	{
+		get => Value.Position;
+		set
+		{
+			if (Position == value)
+				return;
+			OnPropertyChanging();
+			_annotator.SetKeyPointPosition(Value, value);
+			OnPropertyChanged();
+		}
+	}
+
+	protected KeyPointViewModel(PoserAnnotator annotator)
+	{
+		_annotator = annotator;
+	}
+
+	private readonly PoserAnnotator _annotator;
 }
