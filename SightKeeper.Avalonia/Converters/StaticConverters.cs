beb8f5321d0aa6fa2284aeeaaab307d7804fbb96
Neakita <thekatterdev@gmail.com>
2025-02-24 17:20:25 +0500

    Remove unused converters

--- SightKeeper.Avalonia/Converters/StaticConverters.cs
+++ SightKeeper.Avalonia/Converters/StaticConverters.cs
@@ -1,5 +1,4 @@
-﻿using Avalonia.Controls;
-using Avalonia.Data.Converters;
+﻿using Avalonia.Data.Converters;
 
 namespace SightKeeper.Avalonia.Converters;
 
@@ -7,6 +6,4 @@ internal static class StaticConverters
 {
 	public static FuncValueConverter<object, double> ContentToOpacityConverter { get; } =
 		new(value => value == null ? 0 : 1);
-	public static FuncValueConverter<double, GridLength> DoubleToGridLengthConvert { get; } =
-		new(d => new GridLength(d));
 }
