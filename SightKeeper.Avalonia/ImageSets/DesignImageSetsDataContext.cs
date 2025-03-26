82dc8813bdae50ed198d05dca083e09aab5e5491
Neakita <thekatterdev@gmail.com>
2025-03-26 17:15:36 +0500

    Append samples to DesignImageSetsDataContext

--- SightKeeper.Avalonia/ImageSets/DesignImageSetsDataContext.cs
+++ SightKeeper.Avalonia/ImageSets/DesignImageSetsDataContext.cs
@@ -10,8 +10,15 @@ internal class DesignImageSetsDataContext : ImageSetsDataContext
 	public IReadOnlyCollection<ImageSetCardDataContext> ImageSets { get; } =
 	[
 		new DesignImageSetCardDataContext("PD2"),
-		new DesignImageSetCardDataContext("KF2"),
-		new DesignImageSetCardDataContext("Some dataset specific set")
+		new DesignImageSetCardDataContext("KF2 Sample 1", "kfSample1.jpg"),
+		new DesignImageSetCardDataContext("KF2 Sample 2", "kfSample2.jpg"),
+		new DesignImageSetCardDataContext("KF2 Sample 3", "kfSample3.jpg"),
+		new DesignImageSetCardDataContext("KF2 Sample 4", "kfSample4.jpg"),
+		new DesignImageSetCardDataContext("KF2 Sample 5", "kfSample5.jpg"),
+		new DesignImageSetCardDataContext("KF2 Sample 6", "kfSample6.jpg"),
+		new DesignImageSetCardDataContext("KF2 Sample 7", "kfSample7.jpg"),
+		new DesignImageSetCardDataContext("KF2 Sample 8", "kfSample8.jpg"),
+		
 	];
 
 	public ICommand CreateImageSetCommand { get; } = new RelayCommand(() => { });
