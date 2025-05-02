21f63afa3b75e25048c62e380f4c7c3a4f92e5f0
Neakita <thekatterdev@gmail.com>
2025-05-02 05:31:36 +0500

    Clear images at the end of HotKeyScreenCapture.MakeImages loop

--- SightKeeper.Application/ScreenCapturing/HotKeyScreenCapture`1.cs
+++ SightKeeper.Application/ScreenCapturing/HotKeyScreenCapture`1.cs
@@ -13,32 +13,37 @@ public sealed class HotKeyScreenCapture<TPixel> : HotKeyScreenCapture
 		ScreenBoundsProvider screenBoundsProvider,
 		BindingsManager bindingsManager,
 		ImageSaver<TPixel> imageSaver,
-		SelfActivityProvider selfActivityProvider)
+		SelfActivityProvider selfActivityProvider,
+		ImagesCleaner imagesCleaner)
 		: base(screenBoundsProvider, bindingsManager)
 	{
 		_screenCapture = screenCapture;
 		_imageSaver = imageSaver;
 		_selfActivityProvider = selfActivityProvider;
+		_imagesCleaner = imagesCleaner;
 	}
 
 	private readonly ScreenCapture<TPixel> _screenCapture;
 	private readonly ImageSaver<TPixel> _imageSaver;
 	private readonly SelfActivityProvider _selfActivityProvider;
+	private readonly ImagesCleaner _imagesCleaner;
 
 	protected override void MakeImages(ActionContext context)
 	{
 		if (_selfActivityProvider.IsOwnWindowActive)
 			return;
+		Guard.IsNotNull(Set);
+		var set = Set;
 		var processingStopwatch = Stopwatch.StartNew();
 		do
 		{
 			if (_imageSaver is LimitedSaver limitedSession && context.IsEliminatedAfterCompletion(limitedSession.Limit))
-				return;
+				break;
 			processingStopwatch.Restart();
 			var imageData = _screenCapture.Capture(ImageSize, Offset);
-			Guard.IsNotNull(Set);
-			_imageSaver.SaveImage(Set, imageData);
+			_imageSaver.SaveImage(set, imageData);
 		} while (IsEnabled && context.IsAliveAfter(Timeout - processingStopwatch.Elapsed));
 		processingStopwatch.Stop();
+		_imagesCleaner.RemoveExceedUnusedImages(set);
 	}
 }
