9a32ead289fde1e605f5212f693703d3cd126af0
Neakita <thekatterdev@gmail.com>
2025-05-14 14:36:28 +0500

    Add more AssetsLibraryTests

--- SightKeeper.Domain.Tests/DataSets/AssetsLibraryTests.cs
+++ SightKeeper.Domain.Tests/DataSets/AssetsLibraryTests.cs
@@ -36,6 +36,78 @@ public sealed class AssetsLibraryTests
 		library.Assets.Should().BeEmpty();
 	}
 
+	[Fact]
+	public void ShouldNotDeleteAssetByImageThatWasNotCreated()
+	{
+		var image = Utilities.CreateImage();
+		var library = CreateLibrary();
+		Assert.Throws<ArgumentException>(() => library.DeleteAsset(image));
+	}
+
+	[Fact]
+	public void ShouldNotDeleteAssetByImageTwice()
+	{
+		var image = Utilities.CreateImage();
+		var library = CreateLibrary();
+		library.MakeAsset(image);
+		library.DeleteAsset(image);
+		Assert.Throws<ArgumentException>(() => library.DeleteAsset(image));
+	}
+
+	[Fact]
+	public void ShouldGetNewAsset()
+	{
+		var image = Utilities.CreateImage();
+		var library = CreateLibrary();
+		var asset = library.GetOrMakeAsset(image);
+		library.Assets.Should().Contain(asset);
+	}
+
+	[Fact]
+	public void ShouldGetExistingAsset()
+	{
+		var image = Utilities.CreateImage();
+		var library = CreateLibrary();
+		var asset = library.MakeAsset(image);
+		var asset2 = library.GetOrMakeAsset(image);
+		library.Assets.Should().HaveCount(1);
+		asset2.Should().Be(asset);
+	}
+
+	[Fact]
+	public void ShouldContainImageAfterAssetCreation()
+	{
+		var image = Utilities.CreateImage();
+		var library = CreateLibrary();
+		library.MakeAsset(image);
+		library.Contains(image).Should().BeTrue();
+	}
+
+	[Fact]
+	public void ShouldNotContainImageForWhichAssetWasNotCreated()
+	{
+		var image = Utilities.CreateImage();
+		var library = CreateLibrary();
+		library.Contains(image).Should().BeFalse();
+	}
+
+	[Fact]
+	public void ShouldNotGetOptionalAssetForImageWhichAssetWasNotCreated()
+	{
+		var image = Utilities.CreateImage();
+		var library = CreateLibrary();
+		library.GetOptionalAsset(image).Should().BeNull();
+	}
+
+	[Fact]
+	public void ShouldGetOptionalAsset()
+	{
+		var image = Utilities.CreateImage();
+		var library = CreateLibrary();
+		var asset = library.MakeAsset(image);
+		library.GetOptionalAsset(image).Should().Be(asset);
+	}
+
 	private static AssetsOwner<Asset> CreateLibrary()
 	{
 		var dataSet = CreateDataSet();
