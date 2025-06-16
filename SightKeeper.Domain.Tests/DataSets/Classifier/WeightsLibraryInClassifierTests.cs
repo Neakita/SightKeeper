210c29b0d5e866f02f63ed19b1fb4820caa369cb
Neakita <thekatterdev@gmail.com>
2025-06-16 13:27:43 +0500

    Fix WeightsLibraryInClassifierTests

--- SightKeeper.Domain.Tests/DataSets/Classifier/WeightsLibraryInClassifierTests.cs
+++ SightKeeper.Domain.Tests/DataSets/Classifier/WeightsLibraryInClassifierTests.cs
@@ -1,5 +1,7 @@
-﻿/*using FluentAssertions;
+﻿using NSubstitute;
 using SightKeeper.Domain.DataSets.Classifier;
+using SightKeeper.Domain.DataSets.Tags;
+using SightKeeper.Domain.DataSets.Weights;
 
 namespace SightKeeper.Domain.Tests.DataSets.Classifier;
 
@@ -8,21 +10,28 @@ public sealed class WeightsLibraryInClassifierTests
 	[Fact]
 	public void ShouldAddWeightsWithTwoTags()
 	{
-		DomainClassifierDataSet dataSet = new();
-		var tag1 = dataSet.TagsLibrary.CreateTag("1");
-		var tag2 = dataSet.TagsLibrary.CreateTag("2");
-		var weights = Utilities.CreateWeights(tag1, tag2);
-		dataSet.WeightsLibrary.AddWeights(weights);
-		dataSet.WeightsLibrary.Weights.Should().Contain(weights);
+		var innerSet = Substitute.For<ClassifierDataSet>();
+		DomainClassifierDataSet domainSet = new(innerSet);
+		var tag1 = Substitute.For<Tag>();
+		tag1.Owner.Returns(domainSet.TagsLibrary);
+		var tag2 = Substitute.For<Tag>();
+		tag2.Owner.Returns(domainSet.TagsLibrary);
+		var weights = Substitute.For<Weights>();
+		weights.Tags.Returns([tag1, tag2]);
+		domainSet.WeightsLibrary.AddWeights(weights);
+		innerSet.WeightsLibrary.Received().AddWeights(weights);
 	}
 
 	[Fact]
 	public void ShouldNotAddWeightsWithOneTag()
 	{
-		DomainClassifierDataSet dataSet = new();
-		var tag = dataSet.TagsLibrary.CreateTag("");
-		var weights = Utilities.CreateWeights(tag);
-		Assert.Throws<ArgumentException>(() => dataSet.WeightsLibrary.AddWeights(weights));
-		dataSet.WeightsLibrary.Weights.Should().BeEmpty();
+		var innerSet = Substitute.For<ClassifierDataSet>();
+		DomainClassifierDataSet domainSet = new(innerSet);
+		var tag = Substitute.For<Tag>();
+		tag.Owner.Returns(domainSet.TagsLibrary);
+		var weights = Substitute.For<Weights>();
+		weights.Tags.Returns([tag]);
+		Assert.Throws<ArgumentException>(() => domainSet.WeightsLibrary.AddWeights(weights));
+		innerSet.WeightsLibrary.DidNotReceive().AddWeights(Arg.Any<Weights>());
 	}
-}*/
+}
