544593d14df1173fb8a71b9f0bf88f30eb1c9ed5
Neakita <thekatterdev@gmail.com>
2025-06-19 17:40:03 +0500

    Don't use AppDataAccess in tests as it uses file system and thus doesn't work properly in tests

--- SightKeeper.Data.Tests/Utilities.cs
+++ SightKeeper.Data.Tests/Utilities.cs
@@ -1,35 +1,13 @@
+using CommunityToolkit.Diagnostics;
+using MemoryPack;
 using NSubstitute;
-using SightKeeper.Application.ScreenCapturing;
 using SightKeeper.Data.Model.Images;
-using SightKeeper.Data.Services;
-using SightKeeper.Domain.DataSets;
 using SightKeeper.Domain.Images;
 
 namespace SightKeeper.Data.Tests;
 
 internal static class Utilities
 {
-	public static void AddImageSetToAppData(DomainImageSet set, AppDataAccess appDataAccess)
-	{
-		AppDataImageSetsRepository repository = new()
-		{
-			AppDataLock = new Lock(),
-			AppDataAccess = appDataAccess,
-			ChangeListener = Substitute.For<ChangeListener>(),
-			ImageDataAccess = Substitute.For<WriteImageDataAccess>()
-		};
-		repository.Add(set);
-	}
-
-	public static void AddDataSetToAppData(DataSet set, AppDataAccess appDataAccess)
-	{
-		AppDataDataSetsRepository repository = new(
-			appDataAccess,
-			Substitute.For<ChangeListener>(),
-			new Lock());
-		repository.Add(set);
-	}
-
 	public static ImageSet CreateImageSet()
 	{
 		var factory = new StorableImageSetFactory(Substitute.For<ChangeListener>(), new Lock());
@@ -39,17 +17,9 @@ internal static class Utilities
 	public static ImageSet Persist(this ImageSet set)
 	{
 		PersistenceBootstrapper.Setup(Substitute.For<ChangeListener>(), new Lock());
-		AppDataAccess appDataAccess = new();
-		AppDataImageSetsRepository repository = new()
-		{
-			AppDataLock = new Lock(),
-			AppDataAccess = appDataAccess,
-			ChangeListener = Substitute.For<ChangeListener>(),
-			ImageDataAccess = Substitute.For<WriteImageDataAccess>()
-		};
-		repository.Add(set);
-		appDataAccess.Save();
-		appDataAccess.Load();
-		return repository.Items.Single();
+		var serialized = MemoryPackSerializer.Serialize(set);
+		var imageSet = MemoryPackSerializer.Deserialize<ImageSet>(serialized);
+		Guard.IsNotNull(imageSet);
+		return imageSet;
 	}
 }
