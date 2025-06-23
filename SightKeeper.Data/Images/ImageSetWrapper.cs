6f81ca247fd9a7857b3df7223ea0e2cf11c51156
Neakita <thekatterdev@gmail.com>
2025-06-23 12:38:28 +0500

    Move image set decorating from StorableImageSet constructor to ImageSetWrapper

--- SightKeeper.Data/Images/ImageSetWrapper.cs
+++ SightKeeper.Data/Images/ImageSetWrapper.cs
@@ -4,8 +4,21 @@ namespace SightKeeper.Data.Images;
 
 internal sealed class ImageSetWrapper(ChangeListener changeListener, Lock editingLock)
 {
-	public ImageSet Wrap(InMemoryImageSet inMemorySet)
+	public ImageSet Wrap(ImageSet set)
 	{
-		return new StorableImageSet(inMemorySet, editingLock, changeListener);
+		var decoratedSet = set
+			// Tracking is locked because we don't want potential double saving when after modifying saving thread will immediately save and consider changes handled,
+			// and then tracking decorator will send another notification.
+			.WithTracking(changeListener)
+			// Locking of domain rules can be relatively computationally heavy,
+			// for example when removing images range every image should be checked if it is used by some asset,
+			// so locking appears only after domain rules validated.
+			.WithLocking(editingLock)
+			// Images observing decorator can also be before domain rules,
+			// but domain rules can often throw exceptions so placing observing decorator after domain rules will make stack trace a bit shorter.
+			// Observer should be able to stream received images, so observable decorator should contain streamable decorator.
+			.WithObservableImages()
+			.WithDomainRules();
+		return new StorableImageSet(decoratedSet);
 	}
 }
--- SightKeeper.Data/Images/StorableImageSet.cs
+++ SightKeeper.Data/Images/StorableImageSet.cs
@@ -9,80 +9,61 @@ namespace SightKeeper.Data.Images;
 // I wanted to use series of decorators so each decorator does small part of the job,
 // but I considered it will be not obvious to cast instance through some method and not language casting operators,
 // so interface(s) should be implemented by single class
-internal sealed class StorableImageSet : ImageSet, INotifyPropertyChanged
+internal sealed class StorableImageSet(ImageSet inner) : ImageSet, INotifyPropertyChanged
 {
 	public event PropertyChangedEventHandler? PropertyChanged;
 
 	public string Name
 	{
-		get => _decorated.Name;
+		get => inner.Name;
 		set
 		{
 			if (value == Name)
 				return;
-			_decorated.Name = value;
+			inner.Name = value;
 			OnPropertyChanged();
 		}
 	}
 
 	public string Description
 	{
-		get => _decorated.Description;
+		get => inner.Description;
 		set
 		{
 			if (value == Description)
 				return;
-			_decorated.Description = value;
+			inner.Description = value;
 			OnPropertyChanged();
 		}
 	}
 
-	public IReadOnlyList<Image> Images => _decorated.Images;
-
-	public StorableImageSet(ImageSet inner, Lock editingLock, ChangeListener changeListener)
-	{
-		_decorated = inner
-			// Tracking is locked because we don't want potential double saving when after modifying saving thread will immediately save and consider changes handled,
-			// and then tracking decorator will send another notification.
-			.WithTracking(changeListener)
-			// Locking of domain rules can be relatively computationally heavy,
-			// for example when removing images range every image should be checked if it is used by some asset,
-			// so locking appears only after domain rules validated.
-			.WithLocking(editingLock)
-			// Images observing decorator can also be before domain rules,
-			// but domain rules can often throw exceptions so placing observing decorator after domain rules will make stack trace a bit shorter.
-			// Observer should be able to stream received images, so observable decorator should contain streamable decorator.
-			.WithObservableImages()
-			.WithDomainRules();
-	}
+	public IReadOnlyList<Image> Images => inner.Images;
 
 	public Image CreateImage(DateTimeOffset creationTimestamp, Vector2<ushort> size)
 	{
-		return _decorated.CreateImage(creationTimestamp, size);
+		return inner.CreateImage(creationTimestamp, size);
 	}
 
 	public IReadOnlyList<Image> GetImagesRange(int index, int count)
 	{
 		// read can be performed directly from packable instance, not a big deal.
-		return _decorated.GetImagesRange(index, count);
+		return inner.GetImagesRange(index, count);
 	}
 
 	public int IndexOf(Image image)
 	{
-		return _decorated.IndexOf(image);
+		return inner.IndexOf(image);
 	}
 
 	public void RemoveImageAt(int index)
 	{
-		_decorated.RemoveImageAt(index);
+		inner.RemoveImageAt(index);
 	}
 
 	public void RemoveImagesRange(int index, int count)
 	{
-		_decorated.RemoveImagesRange(index, count);
+		inner.RemoveImagesRange(index, count);
 	}
-
-	private readonly ImageSet _decorated;
 
 	private void OnPropertyChanged([CallerMemberName] string propertyName = "")
 	{
