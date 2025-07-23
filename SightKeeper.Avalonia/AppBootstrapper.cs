26fe7cd3b11ef2a77cd697af991997cc5c581545
Neakita <thekatterdev@gmail.com>
2025-07-24 00:29:20 +0500

    Remove redundant logger initialization in AppBootstrapper

--- SightKeeper.Avalonia/AppBootstrapper.cs
+++ SightKeeper.Avalonia/AppBootstrapper.cs
@@ -8,19 +8,10 @@ internal static class AppBootstrapper
 {
 	public static Composition Setup()
 	{
-		SetupLogger();
 		Composition composition = new();
 		Log.Verbose("Composition:\n{composition}", composition.ToString());
 		SetupPersistence(composition);
 		return composition;
-	}
-
-	private static void SetupLogger()
-	{
-		Log.Logger = new LoggerConfiguration()
-			.MinimumLevel.Verbose()
-			.WriteTo.Debug()
-			.CreateLogger();
 	}
 
 	private static void SetupPersistence(Composition composition)
