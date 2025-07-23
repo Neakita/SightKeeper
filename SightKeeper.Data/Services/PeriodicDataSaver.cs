fed3078c73047d0ff304619d551f3165cf76e0b5
Neakita <thekatterdev@gmail.com>
2025-07-24 00:34:23 +0500

    Add exceptions handling and logging to PeriodicDataSaver

--- SightKeeper.Data/Services/PeriodicDataSaver.cs
+++ SightKeeper.Data/Services/PeriodicDataSaver.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 namespace SightKeeper.Data.Services;
 
 public sealed class PeriodicDataSaver : ChangeListener, IDisposable
@@ -8,9 +10,10 @@ public sealed class PeriodicDataSaver : ChangeListener, IDisposable
 		set => _timer.Period = value;
 	}
 
-	public PeriodicDataSaver(DataSaver dataSaver)
+	public PeriodicDataSaver(DataSaver dataSaver, ILogger logger)
 	{
 		_dataSaver = dataSaver;
+		_logger = logger;
 		Start();
 	}
 
@@ -28,22 +31,39 @@ public sealed class PeriodicDataSaver : ChangeListener, IDisposable
 	}
 
 	private readonly DataSaver _dataSaver;
+	private readonly ILogger _logger;
 	private readonly PeriodicTimer _timer = new(TimeSpan.FromSeconds(1));
 	private bool _dataChangedSinceLastSave;
 	private bool _disposed;
 
 	private void Start()
 	{
-		Task.Run(SavePeriodically);
+		var factory = new TaskFactory(TaskCreationOptions.LongRunning, TaskContinuationOptions.None);
+		factory.StartNew(SavePeriodically);
 	}
 
 	private async Task SavePeriodically()
 	{
 		while (await _timer.WaitForNextTickAsync())
+			SaveIfNecessary();
+	}
+
+	private void SaveIfNecessary()
+	{
+		if (_dataChangedSinceLastSave)
+			SaveData();
+		_dataChangedSinceLastSave = false;
+	}
+
+	private void SaveData()
+	{
+		try
 		{
-			if (_dataChangedSinceLastSave)
-				_dataSaver.Save();
-			_dataChangedSinceLastSave = false;
+			_dataSaver.Save();
+		}
+		catch (Exception exception)
+		{
+			_logger.Error(exception, "An exception was thrown when trying to save the data");
 		}
 	}
 }
