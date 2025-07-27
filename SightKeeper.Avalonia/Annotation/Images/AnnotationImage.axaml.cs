3c096e06443c48b967fb7927445e8909ab00047f
Neakita <thekatterdev@gmail.com>
2025-07-27 21:56:59 +0500

    Encapsulate AnnotationImage view from Images view

---
+++ SightKeeper.Avalonia/Annotation/Images/AnnotationImage.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SightKeeper.Avalonia.Annotation.Images;
+
+public sealed partial class AnnotationImage : UserControl
+{
+	public AnnotationImage()
+	{
+		InitializeComponent();
+	}
+}
