44daead4eaf6195be662f4f393b07c0aa2d09aae
Neakita <thekatterdev@gmail.com>
2025-07-29 15:02:55 +0500

    Update comments and formatting in StorableImageSetWrapper

--- SightKeeper.Data/ImageSets/StorableImageSetWrapper.cs
+++ SightKeeper.Data/ImageSets/StorableImageSetWrapper.cs
@@ -5,23 +5,31 @@ public sealed class StorableImageSetWrapper(ChangeListener changeListener, Lock 
 	public StorableImageSet Wrap(StorableImageSet set)
 	{
 		return set
+
 			// Tracking is locked because we don't want potential double saving when after modifying saving thread will immediately save and consider changes handled,
 			// and then tracking decorator will send another notification.
 			.WithTracking(changeListener)
+
 			// Locking of domain rules can be relatively computationally heavy,
 			// for example when removing images range every image should be checked if it is used by some asset,
 			// so locking appears only after domain rules validated.
 			.WithLocking(editingLock)
+
 			// Images data removing could be expansive (we can remove hundreds or thousands of images in one call, or do that often),
 			// and there is no need in lock because lock should affect AppData only,
 			// not the image files,
 			// so it shouldn't be locked
 			.WithImagesDataRemoving()
+
 			// Images observing decorator can also be before domain rules,
-			// but domain rules can often throw exceptions so placing observing decorator after domain rules will make stack trace a bit shorter.
-			// Observer should be able to stream received images, so observable decorator should contain streamable decorator.
+			// but domain rules can often throw exceptions
+			// so placing observing decorator after domain rules will make stack trace a bit shorter.
 			.WithObservableImages()
+
+			// If domain rule is violated and throws an exception,
+			// it should fail as fast as possible and have smaller stack strace
 			.WithDomainRules()
+
 			// INPC interface should be exposed to consumer,
 			// so he can type test and cast it,
 			// so it should be the outermost layer
