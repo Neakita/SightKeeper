eeff74e74ace7732411f7bda1e2baa18fa0b332d
Neakita <thekatterdev@gmail.com>
2025-08-02 05:19:12 +0500

    Raise DeleteSelectedImageCommand.CanExecuteChanged when selected image assets changed

--- SightKeeper.Avalonia/Annotation/Tooling/Commands/DeleteSelectedImageCommand.cs
+++ SightKeeper.Avalonia/Annotation/Tooling/Commands/DeleteSelectedImageCommand.cs
@@ -1,20 +1,27 @@
 using System;
+using System.Reactive;
+using System.Reactive.Linq;
 using CommunityToolkit.Diagnostics;
 using SightKeeper.Avalonia.Annotation.Images;
 using SightKeeper.Avalonia.Misc;
+using SightKeeper.Domain.Images;
 
 namespace SightKeeper.Avalonia.Annotation.Tooling.Commands;
 
 public sealed class DeleteSelectedImageCommand : Command, IDisposable
 {
-	protected override bool CanExecute => _imageSelection.SelectedImage?.Assets.Count == 0;
-
 	public DeleteSelectedImageCommand(ImageSelection imageSelection, ImageSetSelection imageSetSelection)
 	{
 		_imageSelection = imageSelection;
 		_imageSetSelection = imageSetSelection;
-		_constructorDisposable = imageSelection.SelectedImageChanged.Subscribe(_ => RaiseCanExecuteChanged());
+		var assetsChanged = _imageSelection.SelectedImageChanged.Select(GetAssetsObservable).Switch();
+		_constructorDisposable = imageSelection.SelectedImageChanged
+			.Select(_ => Unit.Default)
+			.Merge(assetsChanged)
+			.Subscribe(_ => RaiseCanExecuteChanged());
 	}
+
+	protected override bool CanExecute => _imageSelection.SelectedImage?.Assets.Count == 0;
 
 	protected override void Execute()
 	{
@@ -31,4 +38,11 @@ public sealed class DeleteSelectedImageCommand : Command, IDisposable
 	private readonly ImageSelection _imageSelection;
 	private readonly ImageSetSelection _imageSetSelection;
 	private readonly IDisposable _constructorDisposable;
+
+	private static IObservable<Unit> GetAssetsObservable(Image? image)
+	{
+		if (image?.Assets is IObservable<object> assetsObservable)
+			return assetsObservable.Select(_ => Unit.Default);
+		return Observable.Empty<Unit>();
+	}
 }
