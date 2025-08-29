a73abffdfdad92e199b1d4b90185002861f34e8e
Katter <narcat99@gmail.com>
2025-08-29 12:33:44 +0500

    Fix DFineDetectionConfigurator dataset paths accordingly to COCO exporter behavior

--- SightKeeper.Application/Training/DFINE/DFineDetectionConfigurator.cs
+++ SightKeeper.Application/Training/DFINE/DFineDetectionConfigurator.cs
@@ -11,21 +11,21 @@ internal sealed class DFineDetectionConfigurator(string repositoryDirectoryPath,
 
 		// including background
 		tagsCount++;
-		
+
 		configLines.ReplaceAt(6, "777", tagsCount.ToString());
 		configLines.ReplaceAt(13, "/data/yourdataset/train", TrainImagesDirectoryPath);
 		configLines.ReplaceAt(14, "/data/yourdataset/train/train.json", TrainDataSetFilePath);
 		configLines.ReplaceAt(30, "/data/yourdataset/val", ValidationImagesDirectoryPath);
 		configLines.ReplaceAt(31, "/data/yourdataset/val/val.json", ValidationDataSetFilePath);
-		
+
 		await File.WriteAllLinesAsync(DetectionConfigPath, configLines, cancellationToken);
 	}
 
 	private string ConfigsDirectoryPath => Path.Combine(repositoryDirectoryPath, "configs");
 	private string DetectionConfigPath => Path.Combine(ConfigsDirectoryPath, "dataset", "custom_detection.yml");
 	private string OriginalDetectionConfigPath => Path.Combine(ConfigsDirectoryPath, "dataset", "original_custom_detection.yml");
-	private string TrainImagesDirectoryPath => Path.Combine(dataSetDirectoryPath, "train");
-	private string TrainDataSetFilePath => Path.Combine(dataSetDirectoryPath, "train.json");
-	private string ValidationImagesDirectoryPath => Path.Combine(dataSetDirectoryPath, "validation");
-	private string ValidationDataSetFilePath => Path.Combine(dataSetDirectoryPath, "validation.json");
+	private string TrainImagesDirectoryPath => Path.Combine(dataSetDirectoryPath, "train", "images");
+	private string TrainDataSetFilePath => Path.Combine(dataSetDirectoryPath, "train", "data.json");
+	private string ValidationImagesDirectoryPath => Path.Combine(dataSetDirectoryPath, "validation", "images");
+	private string ValidationDataSetFilePath => Path.Combine(dataSetDirectoryPath, "validation", "data.json");
 }
