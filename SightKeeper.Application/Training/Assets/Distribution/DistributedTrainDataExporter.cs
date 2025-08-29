7acd4e98c572937d6289bdb18a1e34a45b184cd9
Katter <narcat99@gmail.com>
2025-08-29 12:43:01 +0500

    Add DistributedTrainDataExporter

---
+++ SightKeeper.Application/Training/Assets/Distribution/DistributedTrainDataExporter.cs
@@ -0,0 +1,21 @@
+﻿using SightKeeper.Application.Training.Data;
+
+namespace SightKeeper.Application.Training.Assets.Distribution;
+
+public sealed class DistributedTrainDataExporter<TAsset>(TrainDataExporter<TAsset> inner) : TrainDataExporter<TAsset> where TAsset : AssetData
+{
+	public AssetsDistributionRequest DistributionRequest { get; set; } = new()
+	{
+		TrainFraction = .90f,
+		ValidationFraction = .05f,
+		TestFraction = .05f
+	};
+
+	public async Task ExportAsync(string path, TrainData<TAsset> data, CancellationToken cancellationToken)
+	{
+		var distribution = AssetsDistributor.DistributeAssets(data.Assets, DistributionRequest);
+		await inner.ExportAsync(Path.Combine(path, "train"), new TrainDataValue<TAsset>(data.Tags, distribution.TrainAssets), cancellationToken);
+		await inner.ExportAsync(Path.Combine(path, "validation"), new TrainDataValue<TAsset>(data.Tags, distribution.ValidationAssets), cancellationToken);
+		await inner.ExportAsync(Path.Combine(path, "test"), new TrainDataValue<TAsset>(data.Tags, distribution.TestAssets), cancellationToken);
+	}
+}
