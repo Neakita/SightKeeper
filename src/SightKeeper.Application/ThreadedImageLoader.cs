1aea5077767a10e9524122b44e9cc4609484f8b5
Katter <narcat99@gmail.com>
2025-09-02 20:53:25 +0500

    Handle exceptions in ThreadedImageLoader

--- src/SightKeeper.Application/ThreadedImageLoader.cs
+++ src/SightKeeper.Application/ThreadedImageLoader.cs
@@ -10,7 +10,7 @@ public sealed class ThreadedImageLoader<TPixel> : ImageLoader<TPixel>
 	public ThreadedImageLoader(ImageLoader<TPixel> inner)
 	{
 		_inner = inner;
-		_ = Task.Run(LoadImagesAsync);
+		Task.Run(ProcessRequestsAsync);
 	}
 
 	public Task<bool> LoadImageAsync(ImageData imageData, Memory<TPixel> target, CancellationToken cancellationToken)
@@ -23,13 +23,13 @@ public sealed class ThreadedImageLoader<TPixel> : ImageLoader<TPixel>
 				Logger.Verbose("Image loading was canceled by cancellationToken registered callback");
 		}, completionSource);
 		var request = new LoadImageRequest(imageData, target, completionSource, cancellationToken);
-		bool isWritten = _channel.Writer.TryWrite(request);
+		bool isWritten = _requestsChannel.Writer.TryWrite(request);
 		Guard.IsTrue(isWritten);
 		return completionSource.Task;
 	}
 
 	private static readonly ILogger Logger = Log.Logger.ForContext<ThreadedImageLoader<TPixel>>();
-	private readonly Channel<LoadImageRequest> _channel = Channel.CreateUnbounded<LoadImageRequest>();
+	private readonly Channel<LoadImageRequest> _requestsChannel = Channel.CreateUnbounded<LoadImageRequest>();
 	private readonly ImageLoader<TPixel> _inner;
 
 	private sealed record LoadImageRequest(
@@ -38,27 +38,33 @@ public sealed class ThreadedImageLoader<TPixel> : ImageLoader<TPixel>
 		TaskCompletionSource<bool> CompletionSource,
 		CancellationToken CancellationToken);
 
-	private async Task LoadImagesAsync()
+	private async Task ProcessRequestsAsync()
 	{
-		while (await _channel.Reader.WaitToReadAsync())
-		while (_channel.Reader.TryRead(out var request))
+		while (await _requestsChannel.Reader.WaitToReadAsync())
+		while (_requestsChannel.Reader.TryRead(out var request))
+			await TryProcessRequestAsync(request);
+	}
+
+	private async Task TryProcessRequestAsync(LoadImageRequest request)
+	{
+		try
 		{
-			if (request.CancellationToken.IsCancellationRequested)
-				continue;
-			bool isLoaded;
-			try
-			{
-				isLoaded = await _inner.LoadImageAsync(request.Image, request.Target, request.CancellationToken);
-			}
-			catch (Exception exception)
-			{
-				bool isSet = request.CompletionSource.TrySetException(exception);
-				if (!isSet)
-					Logger.Warning(exception, "An exception was thrown while loading an image {image} but could not be assigned to the TaskCompletionSource", request.Image);
-				return;
-			}
-			if (request.CompletionSource.TrySetResult(isLoaded))
-				Logger.Verbose("Threaded image {image} loading result set: {isLoaded}", request.Image, isLoaded);
+			await ProcessRequestAsync(request);
 		}
+		catch (Exception exception)
+		{
+			var isExceptionSet = request.CompletionSource.TrySetException(exception);
+			if (!isExceptionSet)
+				Log.Logger.Warning(exception, "An exception was thrown while processing image {image} load request, but couldn't be set to CompletionSource", request.Image);
+		}
+	}
+
+	private async Task ProcessRequestAsync(LoadImageRequest request)
+	{
+		if (request.CancellationToken.IsCancellationRequested)
+			return;
+		var isLoaded = await _inner.LoadImageAsync(request.Image, request.Target, request.CancellationToken);
+		if (request.CompletionSource.TrySetResult(isLoaded))
+			Logger.Verbose("Threaded image {image} loading result set: {isLoaded}", request.Image, isLoaded);
 	}
 }
