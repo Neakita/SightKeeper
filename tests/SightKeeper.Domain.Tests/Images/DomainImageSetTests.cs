512d17fb25e0e81caa317b9b84101ea04f82d0b3
Katter <narcat99@gmail.com>
2025-09-08 03:57:48 +0500

    Add DomainImageSet Name/Description tests

--- tests/SightKeeper.Domain.Tests/Images/DomainImageSetTests.cs
+++ tests/SightKeeper.Domain.Tests/Images/DomainImageSetTests.cs
@@ -102,4 +102,44 @@ public sealed class DomainImageSetTests
 		domainSet.Dispose();
 		innerSet.Received().Dispose();
 	}
+
+	[Fact]
+	public void ShouldGetNameFromInner()
+	{
+		var innerSet = Substitute.For<ImageSet>();
+		const string name = "the name";
+		innerSet.Name.Returns(name);
+		var domainSet = new DomainImageSet(innerSet);
+		domainSet.Name.Returns(name);
+	}
+
+	[Fact]
+	public void ShouldSetNameToInner()
+	{
+		var innerSet = Substitute.For<ImageSet>();
+		const string name = "the name";
+		var domainSet = new DomainImageSet(innerSet);
+		domainSet.Name = name;
+		innerSet.Received().Name = name;
+	}
+
+	[Fact]
+	public void ShouldGetDescriptionFromInner()
+	{
+		var innerSet = Substitute.For<ImageSet>();
+		const string description = "the description";
+		innerSet.Description.Returns(description);
+		var domainSet = new DomainImageSet(innerSet);
+		domainSet.Description.Returns(description);
+	}
+
+	[Fact]
+	public void ShouldSetDescriptionToInner()
+	{
+		var innerSet = Substitute.For<ImageSet>();
+		const string description = "the description";
+		var domainSet = new DomainImageSet(innerSet);
+		domainSet.Description = description;
+		innerSet.Received().Description = description;
+	}
 }
