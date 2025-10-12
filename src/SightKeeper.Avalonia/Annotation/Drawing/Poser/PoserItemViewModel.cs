8e7d14c115a9f2c448474459106c880c7fcd418d
Katter <narcat99@gmail.com>
2025-10-13 00:24:49 +0500

    Make PoserItemViewModel manage its own key points

--- src/SightKeeper.Avalonia/Annotation/Drawing/Poser/PoserItemViewModel.cs
+++ src/SightKeeper.Avalonia/Annotation/Drawing/Poser/PoserItemViewModel.cs
@@ -1,32 +1,31 @@
-using System.Collections.Generic;
-using Avalonia.Collections;
-using CommunityToolkit.Diagnostics;
+using System;
 using SightKeeper.Avalonia.Annotation.Drawing.Bounded;
 using SightKeeper.Domain.DataSets.Poser;
+using Vibrance;
+using Vibrance.Changes;
 
 namespace SightKeeper.Avalonia.Annotation.Drawing.Poser;
 
-public sealed class PoserItemViewModel : BoundedItemViewModel
+public sealed class PoserItemViewModel : BoundedItemViewModel, IDisposable
 {
 	public override PoserItem Value { get; }
 	public override PoserTag Tag => Value.Tag;
-	public IReadOnlyList<KeyPointViewModel> KeyPoints => _keyPoints;
+	public ReadOnlyObservableList<KeyPointViewModel> KeyPoints { get; }
 
 	public PoserItemViewModel(PoserItem value)
 	{
 		Value = value;
+		var observableKeyPoints = (ReadOnlyObservableList<KeyPoint>)value.KeyPoints;
+		KeyPoints = observableKeyPoints.Transform(CreateKeyPointViewModel).ToObservableList();
 	}
 
-	internal void AddKeyPoint(KeyPointViewModel keyPoint)
+	public void Dispose()
 	{
-		_keyPoints.Add(keyPoint);
+		KeyPoints.Dispose();
 	}
 
-	internal void RemoveKeyPoint(KeyPointViewModel keyPoint)
+	private KeyPointViewModel CreateKeyPointViewModel(KeyPoint keyPoint)
 	{
-		var isRemoved = _keyPoints.Remove(keyPoint);
-		Guard.IsTrue(isRemoved);
+		return new KeyPointViewModel(this, keyPoint);
 	}
-
-	private readonly AvaloniaList<KeyPointViewModel> _keyPoints = new();
 }
