7d0605bd93f387946faca2833466f261aaa45950
Katter <narcat99@gmail.com>
2025-10-20 05:20:53 +0500

    Acquire editing lock in MemoryPackDataSaver

--- src/SightKeeper.Data/Services/MemoryPackDataSaver.cs
+++ src/SightKeeper.Data/Services/MemoryPackDataSaver.cs
@@ -9,6 +9,7 @@ using SightKeeper.Domain.Images;
 namespace SightKeeper.Data.Services;
 
 internal sealed class MemoryPackDataSaver(
+	Lock editingLock,
 	Serializer<IReadOnlyCollection<ImageSet>> imageSetsSerializer,
 	ReadRepository<ImageSet> imageSetsRepository,
 	Serializer<IReadOnlyCollection<DataSet<Tag, Asset>>> dataSetsSerializer,
@@ -31,8 +32,11 @@ internal sealed class MemoryPackDataSaver(
 		using var state = MemoryPackWriterOptionalStatePool.Rent(MemoryPackSerializerOptions.Default);
 		var writer = new MemoryPackWriter<ArrayBufferWriter<byte>>(ref bufferWriter, state);
 		writer.WriteUnmanaged(SchemaVersion);
-		imageSetsSerializer.Serialize(ref writer, imageSetsRepository.Items);
-		dataSetsSerializer.Serialize(ref writer, dataSetsRepository.Items);
+		lock (editingLock)
+		{
+			imageSetsSerializer.Serialize(ref writer, imageSetsRepository.Items);
+			dataSetsSerializer.Serialize(ref writer, dataSetsRepository.Items);
+		}
 		writer.Flush();
 		return bufferWriter.WrittenSpan.ToArray();
 	}
