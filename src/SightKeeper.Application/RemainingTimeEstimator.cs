334a13adb3928bd8841986fdb3e40d4d8559e815
Katter <thekatterdev@gmail.com>
2025-11-10 07:27:52 +0500

    Add missing RemaingTimeEstimator class

---
+++ src/SightKeeper.Application/RemainingTimeEstimator.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+
+namespace SightKeeper.Application;
+
+internal sealed class RemainingTimeEstimator(double finalProgress)
+{
+	public TimeSpan? Estimate(double progress)
+	{
+		if (progress == 0)
+			return null;
+		var elapsedTime = _stopwatch.Elapsed;
+		var remainingProgress = finalProgress - progress;
+		var remainingTime = elapsedTime / progress * remainingProgress;
+		return remainingTime;
+	}
+
+	private readonly Stopwatch _stopwatch = Stopwatch.StartNew();
+}
